bf3fe90785b8992412bd1dbe98ff5b943a7e8875
Brandon Welch <brandon.welch@semanticbits.com>
2022-02-28 16:28:22 -0600

    updates to tests per comments made on 2/25

--- cypress/Shared/MeasureGroupPage.ts
+++ cypress/Shared/MeasureGroupPage.ts
@@ -7,7 +7,7 @@ export class MeasureGroupPage {
     public static readonly initialPopulation1 = ""
 
     //saved message
-    public static readonly successfulSaveMeasureGroupMsg = '#main > div > div > div > form > div:nth-child(1) > div.sc-jObWnj.knmXkm > div > div.MuiAlert-message.css-1w0ym84'
+    public static readonly successfulSaveMeasureGroupMsg = 'div[data-testid=success-alert] .MuiAlert-message'
 
     public static clickMeasureGroupTab(): void {
         let measureGroupTabValue = EditMeasurePage.measureGroupsTab
