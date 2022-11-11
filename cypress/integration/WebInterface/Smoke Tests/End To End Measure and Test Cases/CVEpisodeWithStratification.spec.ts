fa41632452cf52129ac66d3072461fb21a87fa75
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2022-11-11 11:02:46 -0500

    MAT-4963

---
+++ cypress/integration/WebInterface/Smoke Tests/End To End Measure and Test Cases/CVEpisodeWithStratification.spec.ts
@@ -0,0 +1,172 @@
+import {TestCaseJson} from "../../../../Shared/TestCaseJson"
+import {OktaLogin} from "../../../../Shared/OktaLogin"
+import {CreateMeasurePage} from "../../../../Shared/CreateMeasurePage"
+import {TestCasesPage} from "../../../../Shared/TestCasesPage"
+import {Utilities} from "../../../../Shared/Utilities"
+import {MeasuresPage} from "../../../../Shared/MeasuresPage"
+import {EditMeasurePage} from "../../../../Shared/EditMeasurePage"
+import {CQLEditorPage} from "../../../../Shared/CQLEditorPage"
+import {MeasureGroupPage} from "../../../../Shared/MeasureGroupPage"
+
+let measureName = 'CVEpisodeWithStratification' + Date.now()
+let CqlLibraryName = 'CVEpisodeWithStratification' + Date.now()
+let testCaseTitle = 'PASS'
+let testCaseDescription = 'PASS' + Date.now()
+let testCaseSeries = 'SBTestSeries'
+let testCaseJson = TestCaseJson.CVEpisodeWithStratification_PASS
+let measureCQL = 'library CVEpisodeWithStratification version \'0.0.000\'\n' +
+    'using QICore version \'4.1.1\'\n' +
+    'include FHIRHelpers version \'4.2.000\' called FHIRHelpers\n' +
+    'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\'\n' +
+    'valueset "Annual Wellness Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1240\'\n' +
+    'valueset "Preventive Care Services - Established Office Visit, 18 and Up": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1025\'\n' +
+    'valueset "Preventive Care Services-Initial Office Visit, 18 and Up": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1023\'\n' +
+    'valueset "Home Healthcare Services": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1016\'\n' +
+    'valueset "End Stage Renal Disease": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.353\' \n' +
+    '\n' +
+    'parameter "Measurement Period" Interval<DateTime>\n' +
+    'default Interval[@2019-01-01T00:00:00.0, @2020-01-01T00:00:00.0)\n' +
+    '\n' +
+    'context Patient\n' +
+    '\n' +
+    'define "Denominator":\n' +
+    '"Initial Population"\n' +
+    '\n' +
+    'define "Initial Population":\n' +
+    ' "Qualifying Encounters"\n' +
+    ' \n' +
+    'define "Qualifying Encounters":\n' +
+    '(\n' +
+    '[Encounter: "Office Visit"]\n' +
+    'union [Encounter: "Annual Wellness Visit"]\n' +
+    'union [Encounter: "Preventive Care Services - Established Office Visit, 18 and Up"]\n' +
+    'union [Encounter: "Preventive Care Services-Initial Office Visit, 18 and Up"]\n' +
+    'union [Encounter: "Home Healthcare Services"]\n' +
+    ') ValidEncounter\n' +
+    'where \n' +
+    'ValidEncounter.period during "Measurement Period" \n' +
+    'and \n' +
+    'ValidEncounter.isFinishedEncounter()\n' +
+    '\n' +
+    'define fluent function "isFinishedEncounter"(Enc Encounter):\n' +
+    '(Enc E where E.status = \'finished\') is not null\n' +
+    '\n' +
+    'define function MeasureObservation(e Encounter):\n' +
+    '  2\n' +
+    '\n' +
+    'define "Stratification 1":\n' +
+    ' "Qualifying Encounters" Enc\n' +
+    ' where Enc.type in "Annual Wellness Visit"\n' +
+    ' \n' +
+    'define "Stratification 2":\n' +
+    '  "Qualifying Encounters" Enc\n' +
+    ' where Enc.type in "Preventive Care Services - Established Office Visit, 18 and Up"'
+
+describe('Measure Creation and Testing: CV Episode Measure With Stratification', () => {
+
+    before('Create Measure, Test Case and Login', () => {
+
+        OktaLogin.Login()
+        //Create New Measure
+        CreateMeasurePage.CreateQICoreMeasureAPI(measureName, CqlLibraryName, measureCQL, false, false,
+            '2021-01-01', '2022-12-31')
+
+        TestCasesPage.CreateTestCaseAPI(testCaseTitle, testCaseDescription, testCaseSeries, testCaseJson)
+
+        OktaLogin.Login()
+
+    })
+
+    after('Clean up', () => {
+
+        OktaLogin.Logout()
+
+        Utilities.deleteMeasure(measureName, CqlLibraryName)
+
+    })
+
+    it('End to End CV Episode Measure with Stratification, Pass Result', () => {
+
+        //Click on Edit Button
+        MeasuresPage.clickEditforCreatedMeasure()
+
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+
+        //Create Measure Group
+        cy.get(EditMeasurePage.measureGroupsTab).click()
+
+        Utilities.setMeasureGroupType()
+
+        cy.get(MeasureGroupPage.popBasis).should('exist')
+        cy.get(MeasureGroupPage.popBasis).should('be.visible')
+        cy.get(MeasureGroupPage.popBasis).click()
+        cy.get(MeasureGroupPage.popBasis).type('Encounter')
+        cy.get(MeasureGroupPage.popBasisOption).click()
+
+        Utilities.dropdownSelect(MeasureGroupPage.measureScoringSelect, 'Continuous Variable')
+        Utilities.dropdownSelect(MeasureGroupPage.initialPopulationSelect, 'Initial Population')
+        Utilities.dropdownSelect(MeasureGroupPage.measurePopulationSelect, 'Qualifying Encounters')
+        Utilities.dropdownSelect(MeasureGroupPage.cvMeasureObservation, 'MeasureObservation')
+        Utilities.dropdownSelect(MeasureGroupPage.cvAggregateFunction, 'Maximum')
+
+        //Add Stratification fields
+        cy.get(MeasureGroupPage.stratificationTab).should('exist')
+        cy.get(MeasureGroupPage.stratificationTab).click()
+
+        //Add Stratification 1
+        Utilities.dropdownSelect(MeasureGroupPage.stratOne, 'Stratification 1')
+        Utilities.dropdownSelect(MeasureGroupPage.stratAssociationOne, 'initialPopulation')
+        cy.get(MeasureGroupPage.stratDescOne).type('StratificationOne')
+
+        //Add Stratification 2
+        Utilities.dropdownSelect(MeasureGroupPage.stratTwo, 'Stratification 2')
+        Utilities.dropdownSelect(MeasureGroupPage.stratAssociationTwo, 'measurePopulation')
+        cy.get(MeasureGroupPage.stratDescTwo).type('StratificationTwo')
+
+        cy.get(MeasureGroupPage.saveMeasureGroupDetails).should('exist')
+        cy.get(MeasureGroupPage.saveMeasureGroupDetails).should('be.visible')
+        cy.get(MeasureGroupPage.saveMeasureGroupDetails).click()
+
+        //validation successful save message
+        cy.get(MeasureGroupPage.successfulSaveMeasureGroupMsg).should('exist')
+
+        //Navigate to Test Cases page and add Test Case details
+        cy.get(EditMeasurePage.testCasesTab).should('be.visible')
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        cy.get(TestCasesPage.tctExpectedActualSubTab).click()
+        cy.get(TestCasesPage.testCasePopulationList).should('be.visible')
+        cy.get(TestCasesPage.testCaseIPPExpected).should('exist')
+        cy.get(TestCasesPage.testCaseIPPExpected).should('be.enabled')
+        cy.get(TestCasesPage.testCaseIPPExpected).should('be.visible')
+        cy.get(TestCasesPage.testCaseIPPExpected).click()
+        cy.get(TestCasesPage.testCaseIPPExpected).type('1')
+        cy.get(TestCasesPage.testCaseMSRPOPLExpected).type('1')
+        cy.get(TestCasesPage.measureObservationRow).type('2')
+
+        cy.get(TestCasesPage.initialPopulationStratificationExpectedValue).type('1')
+
+        cy.get(TestCasesPage.detailsTab).should('exist')
+        cy.get(TestCasesPage.detailsTab).should('be.visible')
+        cy.get(TestCasesPage.detailsTab).click()
+        cy.get(TestCasesPage.editTestCaseSaveButton).click()
+        cy.get(TestCasesPage.confirmationMsg).should('contain.text', 'Test case updated successfully!')
+
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        cy.get(TestCasesPage.executeTestCaseButton).should('exist')
+        cy.get(TestCasesPage.executeTestCaseButton).should('be.enabled')
+        cy.get(TestCasesPage.executeTestCaseButton).should('be.visible')
+        cy.get(TestCasesPage.executeTestCaseButton).focus()
+        cy.get(TestCasesPage.executeTestCaseButton).invoke('click')
+        cy.get(TestCasesPage.executeTestCaseButton).click()
+        cy.get(TestCasesPage.executeTestCaseButton).click()
+        cy.get(TestCasesPage.testCaseStatus).should('contain.text', 'pass')
+
+    })
+})
