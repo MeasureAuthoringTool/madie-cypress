2bc6add078a98a5ef5cfa14d2fee8a4a5f130846
cvasile001 <57112657+cvasile001@users.noreply.github.com>
2022-11-30 09:07:05 -0500

    More message updates (#647)

--- cypress/integration/WebInterface/Test Cases/Validations/MeasureObservationExpectedValues.spec.ts
+++ cypress/integration/WebInterface/Test Cases/Validations/MeasureObservationExpectedValues.spec.ts
@@ -75,7 +75,7 @@ describe('Measure Observation Expected values', () => {
         cy.get(TestCasesPage.editTestCaseSaveButton).click()
 
         cy.get(TestCasesPage.detailsTab).click()
-        cy.get(TestCasesPage.confirmationMsg).should('contain.text', 'An error occurred with the Test Case JSON while updating the test case')
+        cy.get(TestCasesPage.confirmationMsg).should('contain.text', 'Test case updated successfully with errors in JSON')
 
         //Assert saved observation values
         cy.get(TestCasesPage.tctExpectedActualSubTab).click()
