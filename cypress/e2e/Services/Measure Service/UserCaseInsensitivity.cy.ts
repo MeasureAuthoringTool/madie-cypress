8e3bfe4838ba9971e25c672afdb2ef6a055534fd
Brandon Welch <brandon.welch@semanticbits.com>
2022-12-19 13:33:06 -0600

    Files that were directly affected by my tests for MAT-5099

---
+++ cypress/e2e/Services/Measure Service/UserCaseInsensitivity.cy.ts
@@ -0,0 +1,72 @@
+import { Utilities } from "../../../Shared/Utilities"
+import { MeasureCQL } from "../../../Shared/MeasureCQL"
+import { Environment } from "../../../Shared/Environment"
+import { CreateMeasurePage } from "../../../Shared/CreateMeasurePage"
+import { MeasureGroupPage } from "../../../Shared/MeasureGroupPage"
+import { TestCasesPage } from "../../../Shared/TestCasesPage"
+import { v4 as uuidv4 } from 'uuid'
+
+let measureName = ''
+let CQLLibraryName = ''
+let model = 'QI-Core v4.1.1'
+let harpUser = Environment.credentials().harpUser
+const now = require('dayjs')
+let mpStartDate = now().subtract('1', 'year').format('YYYY-MM-DD')
+let mpEndDate = now().format('YYYY-MM-DD')
+let eCQMTitle = 'eCQMTitle'
+let randValue = (Math.floor((Math.random() * 1000) + 1))
+describe('Measure Service: Create Measure', function () {
+    beforeEach('Set Access Token', function () {
+        cy.setAccessTokenCookie();
+    });
+    after('Clean up', function () {
+        Utilities.deleteMeasure(measureName, CQLLibraryName);
+    });
+    it('Create New Measure, with user name typed in regular case', function () {
+        measureName = 'TestMeasure' + Date.now() + randValue;
+        CQLLibraryName = 'TestCql' + Date.now() + randValue;
+        //create measure
+        cy.getCookie('accessToken').then(function (accessToken) {
+            cy.request({
+                url: '/api/measure',
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer ' + accessToken.value
+                },
+                body: {
+                    "measureName": measureName,
+                    "cqlLibraryName": CQLLibraryName,
+                    "model": model,
+                    "versionId": uuidv4(),
+                    "ecqmTitle": eCQMTitle,
+                    "measurementPeriodStart": mpStartDate,
+                    "measurementPeriodEnd": mpEndDate
+                }
+            }).then(function (response) {
+                expect(response.status).to.eql(201);
+                expect(response.body.createdBy).to.eql(harpUser);
+                cy.writeFile('cypress/fixtures/measureId', response.body.id);
+                cy.writeFile('cypress/fixtures/versionId', response.body.versionId);
+            });
+        });
+        //log in as same user but typed in camel case and confirm that measure can be retrieved and the created by user is the same
+        cy.clearCookies();
+        cy.clearLocalStorage();
+        cy.setAccessTokenCookieCAMELCASE();
+        cy.getCookie('accessToken').then(function (accessToken) {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then(function (id) {
+                cy.request({
+                    url: '/api/measures/' + id,
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value
+                    },
+                    method: 'GET'
+                }).then(function (response) {
+                    expect(response.status).to.eql(200);
+                    expect(response.body.active).to.eql(true);
+                    expect(response.body.content[0].createdBy).to.eql(harpUser);
+                });
+            });
+        });
+    });
+});
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,7 +24,7 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import {Environment} from "../Shared/Environment"
+import { Environment } from "../Shared/Environment"
 
 export { } // this file needs to be a module
 
@@ -38,6 +38,7 @@ declare global {
         interface Chainable {
             setAccessTokenCookie()
             setAccessTokenCookieALT()
+            setAccessTokenCookieCAMELCASE()
             UMLSAPIKeyLogin()
         }
     }
@@ -67,7 +68,8 @@ export function setAccessTokenCookie() {
             'Accept': 'application/json'
         },
 
-        body: { username: Environment.credentials().harpUser,
+        body: {
+            username: Environment.credentials().harpUser,
             password: Environment.credentials().password,
 
             options: {
@@ -145,6 +147,94 @@ export function setAccessTokenCookie() {
     })
 }
 
+export function setAccessTokenCookieCAMELCASE() {
+
+    cy.clearCookies()
+    cy.clearLocalStorage()
+
+    cy.request({
+        url: authnUrl,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept': 'application/json'
+        },
+
+        body: {
+            username: (Environment.credentials().harpUser).toUpperCase,
+            password: (Environment.credentials().password),
+
+            options: {
+                multiOptionalFactorEnroll: false,
+                warnBeforePasswordExpired: true
+            }
+        },
+        failOnStatusCode: false
+    }).then((response) => {
+
+        expect(response.status).to.eql(200)
+        const sessionToken = response.body.sessionToken
+
+        let url = authCodeUrl + '?client_id=' + clientId +
+            '&code_challenge=' + 'LBY2kyC5ZfYC9RaG9HOgRjf9i7U-zgmwHLC280r4UfA' +
+            '&code_challenge_method=S256' +
+            '&response_type=code' +
+            '&response_mode=okta_post_message' +
+            '&display=page' +
+            '&nonce=uxiJab6ycJdNkEZkwbtqnSC1MRuIFCXQATQZSWiBjWdSuuBdbIDCN9EafOYiPaHs' + sessionToken +
+            '&redirect_uri=' + redirectUri +
+            '&sessionToken=' + sessionToken +
+            '&state=iTIppKJsrKTXektB6F1h1dRsQEaDCjlTD3xtjDbYKZ1FlPFKVcq1u7FRuPgPMqxZ' +
+            '&scope=openid%20email%20profile'
+
+        cy.request({
+            url: url,
+            method: 'GET',
+            headers: {
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept': '*/*'
+            }
+        }).then((response) => {
+            expect(response.status).to.eql(200)
+
+            const resp = response.body
+            const codeIdx = resp.indexOf("data.code")
+            const codeEndIdx = resp.indexOf(";", codeIdx)
+            const codeLine = resp.substring(codeIdx, codeEndIdx)
+            const [v, c] = codeLine.split("=")
+            const escapedCode = c.trim().replace(/'/g, '')
+            const authCode = escapedCode.replace(/\\x([0-9A-Fa-f]{2})/g, function () {
+                return String.fromCharCode(parseInt(arguments[1], 16))
+            })
+
+            cy.request({
+                url: tokenUrl,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept-Encoding': 'gzip, deflate, br',
+                    'Accept': '*/*'
+                },
+                body: {
+                    grant_type: 'authorization_code',
+                    client_id: clientId,
+                    redirect_uri: redirectUri,
+                    code: authCode,
+                    code_verifier: codeVerifier
+                }
+            }).then((response) => {
+
+                expect(response.status).to.eql(200)
+                const access_token = response.body.access_token
+                //setting the cookie value to be grabbed for api authentication
+                cy.setCookie('accessToken', access_token)
+
+            })
+        })
+    })
+}
+
 export function setAccessTokenCookieALT() {
 
     cy.clearCookies()
@@ -159,7 +249,8 @@ export function setAccessTokenCookieALT() {
             'Accept': 'application/json'
         },
 
-        body: { username: Environment.credentials().harpUserALT,
+        body: {
+            username: Environment.credentials().harpUserALT,
             password: Environment.credentials().passwordALT,
 
             options: {
@@ -260,34 +351,37 @@ Cypress.Commands.add('setAccessTokenCookie', () => {
 Cypress.Commands.add('setAccessTokenCookieALT', () => {
     return setAccessTokenCookieALT()
 })
+Cypress.Commands.add('setAccessTokenCookieCAMELCASE', () => {
+    return setAccessTokenCookieCAMELCASE()
+})
 Cypress.Commands.add('UMLSAPIKeyLogin', () => {
     return UMLSAPIKeyLogin()
 })
 const compareColor = (color: string, property: keyof CSSStyleDeclaration) => (
     targetEle: JQuery
-  ) => {
+) => {
     const tempEle = document.createElement('div');
     tempEle.style.color = color;
     tempEle.style.display = 'none'; //make sure it doesn't render
     document.body.appendChild(tempEle); //append so that `getComputedStyle` actually works
-  
+
     const tempColor = getComputedStyle(tempEle).color;
     const targetColor = getComputedStyle(targetEle[0])[property];
-  
+
     document.body.removeChild(tempEle); //remove
-  
+
     expect(tempColor).to.equal(targetColor);
-  };
-  
-  Cypress.Commands.overwrite('should', (originalFn: Function, subject: any, expectation: any, ...args: any[]) => {
+};
+
+Cypress.Commands.overwrite('should', (originalFn: Function, subject: any, expectation: any, ...args: any[]) => {
     const customMatchers = {
-      'have.backgroundColor': compareColor(args[0], 'backgroundColor'),
-      'have.color': compareColor(args[0], 'color'),
+        'have.backgroundColor': compareColor(args[0], 'backgroundColor'),
+        'have.color': compareColor(args[0], 'color'),
     };
     //See if the expected is a string and if it's a member of Jest's expect
     if (typeof expectation === 'string' && customMatchers[expectation]) {
-      return originalFn(subject, customMatchers[expectation]);
+        return originalFn(subject, customMatchers[expectation]);
     }
-  
+
     return originalFn(subject, expectation, ...args);
-  });
+});
