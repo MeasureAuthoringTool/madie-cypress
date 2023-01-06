e78b989774470b321d54b61871d34631814df175
Brandon Welch <brandon.welch@semanticbits.com>
2023-01-06 16:12:39 -0600

    Updated existing Service file to include "Bundle" instead of "Bundles" and also updated some minor formatting for the test file, itself.
    Additionally, created a new test file for the new export
    end point.

---
+++ cypress/e2e/Services/Measure Service/MeasureExport.cy.ts
@@ -0,0 +1,188 @@
+import { Utilities } from "../../../Shared/Utilities"
+import { CreateMeasurePage } from "../../../Shared/CreateMeasurePage"
+import { MeasureGroupPage } from "../../../Shared/MeasureGroupPage"
+import { MeasureCQL } from "../../../Shared/MeasureCQL"
+import { OktaLogin } from "../../../Shared/OktaLogin"
+import { MeasuresPage } from "../../../Shared/MeasuresPage"
+import { EditMeasurePage } from "../../../Shared/EditMeasurePage"
+import { v4 as uuidv4 } from 'uuid'
+
+let measureName = 'MeasureBundle' + Date.now()
+let CqlLibraryName = 'MeasureBundle' + Date.now()
+let randValue = (Math.floor((Math.random() * 1000) + 1))
+let newMeasureName = ''
+let newCqlLibraryName = ''
+let newmeasureCQL = MeasureCQL.CQL_Multiple_Populations
+
+let measureCQL = 'library SimpleFhirMeasure version \'0.0.001\'\n' +
+    '\n' +
+    'using FHIR version \'4.0.1\'\n' +
+    '\n' +
+    'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
+    '\n' +
+    'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\'\n' +
+    '\n' +
+    'parameter "Measurement Period" Interval<DateTime>\n' +
+    '\n' +
+    'context Patient\n' +
+    '\n' +
+    'define "ipp":\n' +
+    '  exists ["Encounter": "Office Visit"] E where E.period.start during "Measurement Period" \n' +
+    '  \n' +
+    'define "denom":\n' +
+    '    "ipp"\n' +
+    '    \n' +
+    'define "num":\n' +
+    '    exists ["Encounter"] E where E.status ~ \'finished\'\n' +
+    '      \n' +
+    'define "numeratorExclusion":\n' +
+    '    "num"'
+let CVmeasureCQL = 'library TestLibrary1664888387806162 version \'0.0.000\'\n' +
+    '\n' +
+    'using FHIR version \'4.0.1\'\n' +
+    '\n' +
+    'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
+    '\n' +
+    'parameter "Measurement Period" Interval<DateTime>\n' +
+    '\n' +
+    'context Patient\n' +
+    '\n' +
+    'define "ipp":\n' +
+    '  exists ["Encounter"] E where E.period.start during "Measurement Period"\n' +
+    '  \n' +
+    'define "denom":\n' +
+    '  "ipp"\n' +
+    '  \n' +
+    'define "num":\n' +
+    '  exists ["Encounter"] E where E.status ~ \'finished\'\n' +
+    '  \n' +
+    'define "numeratorExclusion":\n' +
+    '    "num"\n' +
+    '    \n' +
+    'define function ToCode(coding FHIR.Coding):\n' +
+    ' if coding is null then\n' +
+    '   null\n' +
+    '      else\n' +
+    '        System.Code {\n' +
+    '           code: coding.code.value,\n' +
+    '           system: coding.system.value,\n' +
+    '           version: coding.version.value,\n' +
+    '           display: coding.display.value\n' +
+    '           }\n' +
+    '           \n' +
+    'define function fun(notPascalCase Integer ):\n' +
+    '  true\n' +
+    '  \n' +
+    'define function "isFinishedEncounter"():\n' +
+    '  true'
+let PopIniPop = 'ipp'
+let PopNum = 'num'
+let PopDenom = 'denom'
+let PopDenex = 'ipp'
+let PopDenexcep = 'denom'
+let PopNumex = 'numeratorExclusion'
+
+describe('Proportion Measure Bundle end point returns expected data with valid Measure CQL and elmJson', () => {
+
+    beforeEach('Create Measure and set access token', () => {
+
+        newMeasureName = measureName + randValue
+        newCqlLibraryName = CqlLibraryName + randValue
+
+        cy.setAccessTokenCookie()
+
+        //Create New Measure
+        CreateMeasurePage.CreateQICoreMeasureAPI(newMeasureName, newCqlLibraryName, measureCQL)
+
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((retrievedMeasureID) => {
+                cy.request({
+                    url: '/api/measures/' + retrievedMeasureID + '/groups/',
+                    method: 'POST',
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value
+                    },
+                    body: {
+                        "scoring": 'Proportion',
+                        "populationBasis": 'Boolean',
+                        "populations": [
+                            {
+                                "id": uuidv4(),
+                                "name": "initialPopulation",
+                                "definition": PopIniPop
+                            },
+                            {
+                                "id": uuidv4(),
+                                "name": "denominator",
+                                "definition": PopDenom
+                            },
+                            {
+                                "id": uuidv4(),
+                                "name": "denominatorExclusion",
+                                "definition": PopDenex
+                            },
+                            {
+                                "id": uuidv4(),
+                                "name": "denominatorException",
+                                "definition": PopDenexcep
+                            },
+                            {
+                                "id": uuidv4(),
+                                "name": "numerator",
+                                "definition": PopNum
+                            },
+                            {
+                                "id": uuidv4(),
+                                "name": "numeratorExclusion",
+                                "definition": PopNumex
+                            }
+                        ],
+                        "scoringUnit": {
+                            "label": "ml milliLiters",
+                            "value": {
+                                "code": "ml",
+                                "name": "milliLiters",
+                                "guidance": "",
+                                "system": "https://clinicaltables.nlm.nih.gov/"
+                            }
+                        },
+                        "measureGroupTypes": [
+                            "Outcome"
+                        ]
+                    }
+                }).then((response) => {
+                    expect(response.status).to.eql(201)
+                    expect(response.body.id).to.be.exist
+                    cy.writeFile('cypress/fixtures/groupId', response.body.id)
+                })
+            })
+        })
+        cy.setAccessTokenCookie()
+    })
+
+
+    afterEach('Clean up', () => {
+
+        Utilities.deleteMeasure(newMeasureName, newCqlLibraryName)
+
+    })
+
+    it('Confirm the export end point returns a contentType of "application/zip"', () => {
+
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((id) => {
+                cy.request({
+                    url: '/api/measures/' + id + '/exports',
+                    method: 'GET',
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value
+                    }
+                }).then((response) => {
+                    console.log(response)
+                    expect(response.status).to.eql(200)
+                    expect(response.body).is.not.null
+                })
+            })
+        })
+    })
+})
