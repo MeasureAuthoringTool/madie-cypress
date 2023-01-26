622d0becfd4ff4e6e738d6fe7f66b76d233d85f2
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2023-01-26 17:35:00 -0500

    MAT-5152

--- cypress/e2e/WebInterface/Measure/MeasureVersioning/AddVersionToMeasure.cy.ts
+++ cypress/e2e/WebInterface/Measure/MeasureVersioning/AddVersionToMeasure.cy.ts
@@ -1,30 +1,38 @@
 import {MeasureCQL} from "../../../../Shared/MeasureCQL"
 import {CreateMeasurePage} from "../../../../Shared/CreateMeasurePage"
-import {Utilities} from "../../../../Shared/Utilities"
 import {OktaLogin} from "../../../../Shared/OktaLogin"
 import {MeasuresPage} from "../../../../Shared/MeasuresPage"
+import {EditMeasurePage} from "../../../../Shared/EditMeasurePage"
+import {TestCasesPage} from "../../../../Shared/TestCasesPage"
+import {TestCaseJson} from "../../../../Shared/TestCaseJson"
+import {CQLEditorPage} from "../../../../Shared/CQLEditorPage"
+import {MeasureGroupPage} from "../../../../Shared/MeasureGroupPage"
 
 let measureName = 'TestMeasure' + Date.now()
 let cqlLibraryName = 'TestCql' + Date.now()
 let measureCQL = MeasureCQL.SBTEST_CQL
+let versionNumber = '1.0.000'
+let testCaseTitle = 'TestcaseTitle'
+let testCaseDescription = 'Description' + Date.now()
+let testCaseSeries = 'SBTestSeries'
+let testCaseJson = TestCaseJson.CohortEpisodeEncounter_PASS
 
 describe.skip('Measure Versioning', () => {
 
     before('Create Measure and Login', () => {
 
         CreateMeasurePage.CreateQICoreMeasureAPI(measureName, cqlLibraryName, measureCQL)
+        TestCasesPage.CreateTestCaseAPI(testCaseTitle, testCaseDescription, testCaseSeries, testCaseJson)
         OktaLogin.Login()
     })
 
-    after('Logout and Clean up', () => {
+    after('Logout', () => {
 
         OktaLogin.Logout()
-        Utilities.deleteMeasure(measureName, cqlLibraryName)
+
     })
 
-    it('Add Version to the Measure', () => {
-
-        let versionNumber = '1.0.000'
+    it('Add Version to the Measure and verify that the versioned Measure is in read only', () => {
 
         MeasuresPage.clickVersionForCreatedMeasure()
 
@@ -37,5 +45,42 @@ describe.skip('Measure Versioning', () => {
         cy.get(MeasuresPage.measureVersionSuccessMsg).should('contain.text', 'New version of measure is Successfully created')
         MeasuresPage.validateVersionNumber(measureName, versionNumber)
         cy.log('Version Created Successfully')
+
+        //Verify that the fields on Measure details page, CQL Editor page and Test case page are read only
+        MeasuresPage.clickEditforCreatedMeasure()
+
+        //Navigate to Measure details page
+        cy.get(EditMeasurePage.measureNameTextBox).should('not.be.enabled')
+        cy.get(EditMeasurePage.cqlLibraryNameTextBox).should('not.be.enabled')
+
+        cy.get(EditMeasurePage.leftPanelModelAndMeasurementPeriod).click()
+        cy.get(CreateMeasurePage.measurementPeriodStartDate).should('not.be.enabled')
+        cy.get(CreateMeasurePage.measurementPeriodEndDate).should('not.be.enabled')
+
+        //Navigate to CQL Editor page
+        CQLEditorPage.clickCQLEditorTab()
+        cy.get(EditMeasurePage.cqlEditorTextBox).should('not.be.enabled')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).should('not.exist')
+
+        //Navigate to Population Criteria page
+        cy.get(EditMeasurePage.measureGroupsTab).click()
+        cy.get(MeasureGroupPage.popBasis).should('not.be.enabled')
+        cy.get(MeasureGroupPage.measureScoringSelect).should('not.be.enabled')
+        cy.get(MeasureGroupPage.ucumScoringUnitSelect).should('not.be.enabled')
+        cy.get(MeasureGroupPage.saveMeasureGroupDetails).should('not.exist')
+
+        //Navigate to Test cases page
+        cy.get(EditMeasurePage.testCasesTab).click()
+        TestCasesPage.clickEditforCreatedTestCase()
+        cy.get(TestCasesPage.aceEditor).should('not.be.enabled')
+        //Navigate to Details tab
+        cy.get(TestCasesPage.detailsTab).click()
+        cy.get(TestCasesPage.testCaseTitle).should('not.be.enabled')
+        cy.get(TestCasesPage.runTestButton).should('not.be.enabled')
+        cy.get(TestCasesPage.editTestCaseSaveButton).should('not.exist')
+
+        //Verify that the Delete Measure button is disabled
+        cy.get(EditMeasurePage.deleteMeasureButton).should('not.be.enabled')
+
     })
 })
