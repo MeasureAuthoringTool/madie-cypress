ef25abc4dc683af048ff17c611f298d0404108c8
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2023-02-15 16:34:41 -0500

    Test case for default values on Json import and few other updates

---
+++ cypress/e2e/WebInterface/Test Cases/TestCaseImport/DefaultValuesOnImportFile.cy.ts
@@ -0,0 +1,50 @@
+import {CreateMeasurePage} from "../../../../Shared/CreateMeasurePage"
+import {TestCasesPage} from "../../../../Shared/TestCasesPage"
+import {OktaLogin} from "../../../../Shared/OktaLogin"
+import {Utilities} from "../../../../Shared/Utilities"
+import {MeasuresPage} from "../../../../Shared/MeasuresPage"
+import {EditMeasurePage} from "../../../../Shared/EditMeasurePage"
+
+let measureName = 'TestMeasure' + Date.now()
+let CqlLibraryName = 'TestLibrary' + Date.now()
+let testCaseTitle = 'Title for Auto Test'
+let testCaseDescription = 'DENOMFail' + Date.now()
+let testCaseSeries = 'SBTestSeries'
+let fileToUpload = ['NumFail_MedAdminStatus.json', 'DenFail_MedRequestStatus.json', 'ServiceRequest_WithNullStatus.json', 'ServiceRequest_WithNullIntent.json']
+let ValueToBeAdded = ['subject', 'subject', '"status": "active"', '"intent": "order"']
+
+describe('Validate Test case Json on import', () => {
+
+    before('Create Measure, Test case and Login', () => {
+
+        //Create New Measure
+        CreateMeasurePage.CreateQICoreMeasureAPI(measureName, CqlLibraryName)
+        TestCasesPage.CreateTestCaseAPI(testCaseTitle, testCaseDescription, testCaseSeries)
+        OktaLogin.Login()
+
+    })
+
+    after('Clean up and Logout', () => {
+
+        Utilities.deleteMeasure(measureName, CqlLibraryName)
+        OktaLogin.Logout()
+    })
+
+    it('Verify the default values are added to the Test case Json on import', () => {
+
+        //Click on Edit Button
+        MeasuresPage.clickEditforCreatedMeasure()
+
+        //Navigate to Test case list page
+        cy.get(EditMeasurePage.testCasesTab).click()
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        for (let i = 0; i <= 2; i++) {
+
+            TestCasesPage.ImportTestCaseFile(fileToUpload[i])
+
+            TestCasesPage.ValidateValueAddedToTestCaseJson(ValueToBeAdded[i])
+        }
+
+    })
+})
