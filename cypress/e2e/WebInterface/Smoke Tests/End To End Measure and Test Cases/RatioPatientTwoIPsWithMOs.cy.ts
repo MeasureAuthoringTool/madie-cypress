6b2421ba5bd2df7827a18e564d869ed8042aaa64
praveenavas <82400011+praveenavas@users.noreply.github.com>
2023-03-01 16:27:13 -0500

    Removed .skip from tests (#775)
    * MAT-5385
    
    * MAT-5368
    
    * MAT-5360
    
    * Removed .skip from tests

--- cypress/e2e/WebInterface/Smoke Tests/End To End Measure and Test Cases/RatioPatientTwoIPsWithMOs.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/End To End Measure and Test Cases/RatioPatientTwoIPsWithMOs.cy.ts
@@ -59,8 +59,7 @@ let measureCQL = 'library MultipleIPwithObs version \'0.0.000\'\n' +
     'define function "MOMale"():\n' +
     'Patient.gender = \'male\''
 
-//Skipping until link for adding Second Initial Population is fixed, as part of MAT-5373
-describe.skip('Measure Creation and Testing: Ratio Patient Two IPs w/ MOs', () => {
+describe('Measure Creation and Testing: Ratio Patient Two IPs w/ MOs', () => {
 
     before('Create Measure and Test Case', () => {
 
