7474d1ba27c69c286b68c40e72f559b4837411d4
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2023-03-01 16:13:38 -0500

    Removed .skip from tests

--- cypress/e2e/WebInterface/Smoke Tests/End To End Measure and Test Cases/RatioEpisodeTwoIPsWithMOs.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/End To End Measure and Test Cases/RatioEpisodeTwoIPsWithMOs.cy.ts
@@ -58,8 +58,7 @@ let measureCQL = 'library HHSH version \'0.0.000\'\n' +
     '        with TheEncounter E \n' +
     '            such that MedOrder.authoredOn before start of E.period )'
 
-//Skipping until link for adding Second Initial Population is fixed, as part of MAT-5373
-describe.skip('Measure Creation and Testing: Ratio Episode Two IPs w/ MOs', () => {
+describe('Measure Creation and Testing: Ratio Episode Two IPs w/ MOs', () => {
 
     before('Create Measure and Test Case', () => {
 
