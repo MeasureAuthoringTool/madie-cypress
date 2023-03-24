061debbbb636f15d664a1408c558952a4e2dfc88
Brandon Welch <brandon.welch@semanticbits.com>
2023-03-24 10:34:37 -0500

    Validate some things on the export dialog box that is not validated elsewhere.

---
+++ cypress/e2e/WebInterface/Measure/MeasureExport/MeasureExportModal.cy.ts
@@ -0,0 +1,119 @@
+import { CreateMeasurePage } from "../../../../Shared/CreateMeasurePage"
+import { OktaLogin } from "../../../../Shared/OktaLogin"
+import { Utilities } from "../../../../Shared/Utilities"
+import { MeasuresPage } from "../../../../Shared/MeasuresPage"
+import { MeasureGroupPage } from "../../../../Shared/MeasureGroupPage"
+import { EditMeasurePage } from "../../../../Shared/EditMeasurePage"
+import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
+
+let measureNameTimeStamp = 'TestMeasure' + Date.now()
+let measureName = measureNameTimeStamp
+let CqlLibraryName = 'TestLibrary' + Date.now()
+const path = require('path')
+const downloadsFolder = Cypress.config('downloadsFolder')
+const { deleteDownloadsFolderBeforeAll } = require('cypress-delete-downloads-folder')
+
+let measureCQL = 'library TestLibrary1678378360032 version \'0.0.000\'\n' +
+    '\n' +
+    'using QICore version \'4.1.1\'\n' +
+    '\n' +
+    'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
+    'include SupplementalDataElements version \'3.1.000\' called SDE\n' +
+    'include CQMCommon version \'1.0.000\' called CQMCommon \n' +
+    'include FHIRCommon version \'4.1.000\' called FHIRCommon\n' +
+    '\n' +
+    '\n' +
+    'codesystem "SNOMEDCT": \'http://snomed.info/sct\' \n' +
+    'codesystem "ActCode": \'http://terminology.hl7.org/CodeSystem/v3-ActCode\'  \n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    'valueset "Care Services in Long-Term Residential Facility": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1014\' \n' +
+    'valueset "Diabetic Retinopathy": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.327\' \n' +
+    'valueset "Level of Severity of Retinopathy Findings": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1283\' \n' +
+    'valueset "Macular Edema Findings Present": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1320\' \n' +
+    'valueset "Macular Exam": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1251\' \n' +
+    'valueset "Medical Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1007\' \n' +
+    'valueset "Nursing Facility Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1012\' \n' +
+    'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\' \n' +
+    'valueset "Ophthalmological Services": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1285\' \n' +
+    'valueset "Patient Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1008\' \n' +
+    '\n' +
+    'code "Healthcare professional (occupation)": \'223366009\' from "SNOMEDCT" display \'Healthcare professional (occupation)\'\n' +
+    'code "Medical practitioner (occupation)": \'158965000\' from "SNOMEDCT" display \'Medical practitioner (occupation)\'\n' +
+    'code "Ophthalmologist (occupation)": \'422234006\' from "SNOMEDCT" display \'Ophthalmologist (occupation)\'\n' +
+    'code "Optometrist (occupation)": \'28229004\' from "SNOMEDCT" display \'Optometrist (occupation)\'\n' +
+    'code "Physician (occupation)": \'309343006\' from "SNOMEDCT" display \'Physician (occupation)\'\n' +
+    'code "virtual": \'VR\' from "ActCode" display \'virtual\'\n' +
+    'code "Macular edema absent (situation)": \'428341000124108\' from "SNOMEDCT" display \'Macular edema absent (situation)\'\n' +
+    'code "AMB" : \'AMB\' from "ActCode" display \'Ambulatory\'\n' +
+    '\n' +
+    'parameter "Measurement Period" Interval<DateTime>\n' +
+    '\n' +
+    'context Patient\n' +
+    '\n' +
+    'define "SDE Ethnicity":\n' +
+    '  SDE."SDE Ethnicity"\n' +
+    '\n' +
+    'define "SDE Payer":\n' +
+    '  SDE."SDE Payer"\n' +
+    '\n' +
+    'define "SDE Race":\n' +
+    '  SDE."SDE Race"\n' +
+    '\n' +
+    'define "SDE Sex":\n' +
+    '  SDE."SDE Sex"\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    'define "Initial Population":\n' +
+    'true\n' +
+    '\n' +
+    'define "Num":\n' +
+    'true\n' +
+    '\n' +
+    'define "Denom":\n' +
+    'true'
+
+describe('FHIR Measure Export', () => {
+
+    deleteDownloadsFolderBeforeAll()
+
+    before('Create New Measure and Login', () => {
+
+        //Create New Measure
+        CreateMeasurePage.CreateQICoreMeasureAPI(measureName, CqlLibraryName, measureCQL)
+        OktaLogin.Login()
+        MeasuresPage.measureAction("edit")
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{end} {enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        //wait for alert / successful save message to appear
+        Utilities.waitForElementVisible(CQLEditorPage.successfulCQLSaveNoErrors, 40700)
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+        OktaLogin.Logout()
+        //create Measure Group
+        MeasureGroupPage.CreateProportionMeasureGroupAPI(false, false, 'Initial Population',
+            'Num', 'Denom', 'boolean')
+        OktaLogin.Login()
+
+    })
+
+    after('Log out', () => {
+
+        OktaLogin.Logout()
+    })
+
+    it('Validate export modal appears and has spinnner', () => {
+
+        MeasuresPage.measureAction('export')
+
+        //validate that the exporting dialog / modal appears and that the success check mark appears, once export is finished
+        cy.get(MeasuresPage.exportingDialog).should('be.visible')
+        Utilities.waitForElementVisible(MeasuresPage.exportFinishedCheck, 60000)
+        //validate that the exporting dialog / modal window 'continue" button exists, is visible and, then, click on button
+        cy.get(MeasuresPage.exportFinishedContinueBtn).should('exist').should('be.visible')
+        cy.get(MeasuresPage.exportFinishedContinueBtn).click()
+
+    })
+})
