12dc3d3469effc4406119f9c0c0ca9dcad3417d2
Brandon Welch <brandon.welch@semanticbits.com>
2023-04-11 17:33:32 -0500

    Created test that verifies / validates MAT-5442.

--- cypress/e2e/WebInterface/Test Cases/Validations/TestCaseJSON_TerminologyTests.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/Validations/TestCaseJSON_TerminologyTests.cy.ts
@@ -8,6 +8,7 @@ import { Utilities } from "../../../../Shared/Utilities"
 import { MeasureGroupPage } from "../../../../Shared/MeasureGroupPage"
 import { MeasureCQL } from "../../../../Shared/MeasureCQL"
 import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
+import { Header } from "../../../../Shared/Header"
 
 let measureName = 'TestMeasure' + Date.now()
 let CqlLibraryName = 'TestLibrary' + Date.now()
@@ -17,9 +18,72 @@ let missingResourceIDTCJson = TestCaseJson.TestCaseJson_missingResourceIDs
 let dupResourceIDTCJson = TestCaseJson.TestCaseJson_resourceIDsDup
 let missingMetaProfile = TestCaseJson.TestCaseJson_missingMetaProfile
 let validTestCaseJson = TestCaseJson.TestCaseJson_Valid
+let cardInvalidTCJson = TestCaseJson.tcCardErrorJson
 let testCaseSeries = 'SBTestSeries'
 let measureCQL = MeasureCQL.CQL_Multiple_Populations
 let measureCQLPFTests = MeasureCQL.CQL_Populations
+
+let measureCQLCardTests = 'library TestLibrary1678378360032 version \'0.0.000\'\n' +
+    '\n' +
+    'using QICore version \'4.1.1\'\n' +
+    '\n' +
+    'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
+    'include SupplementalDataElements version \'3.1.000\' called SDE\n' +
+    'include CQMCommon version \'1.0.000\' called CQMCommon \n' +
+    'include FHIRCommon version \'4.1.000\' called FHIRCommon\n' +
+    '\n' +
+    '\n' +
+    'codesystem "SNOMEDCT": \'http://snomed.info/sct\' \n' +
+    'codesystem "ActCode": \'http://terminology.hl7.org/CodeSystem/v3-ActCode\'  \n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    'valueset "Care Services in Long-Term Residential Facility": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1014\' \n' +
+    'valueset "Diabetic Retinopathy": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.327\' \n' +
+    'valueset "Level of Severity of Retinopathy Findings": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1283\' \n' +
+    'valueset "Macular Edema Findings Present": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1320\' \n' +
+    'valueset "Macular Exam": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1251\' \n' +
+    'valueset "Medical Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1007\' \n' +
+    'valueset "Nursing Facility Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1012\' \n' +
+    'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\' \n' +
+    'valueset "Ophthalmological Services": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1285\' \n' +
+    'valueset "Patient Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1008\' \n' +
+    '\n' +
+    'code "Healthcare professional (occupation)": \'223366009\' from "SNOMEDCT" display \'Healthcare professional (occupation)\'\n' +
+    'code "Medical practitioner (occupation)": \'158965000\' from "SNOMEDCT" display \'Medical practitioner (occupation)\'\n' +
+    'code "Ophthalmologist (occupation)": \'422234006\' from "SNOMEDCT" display \'Ophthalmologist (occupation)\'\n' +
+    'code "Optometrist (occupation)": \'28229004\' from "SNOMEDCT" display \'Optometrist (occupation)\'\n' +
+    'code "Physician (occupation)": \'309343006\' from "SNOMEDCT" display \'Physician (occupation)\'\n' +
+    'code "virtual": \'VR\' from "ActCode" display \'virtual\'\n' +
+    'code "Macular edema absent (situation)": \'428341000124108\' from "SNOMEDCT" display \'Macular edema absent (situation)\'\n' +
+    'code "AMB" : \'AMB\' from "ActCode" display \'Ambulatory\'\n' +
+    '\n' +
+    'parameter "Measurement Period" Interval<DateTime>\n' +
+    '\n' +
+    'context Patient\n' +
+    '\n' +
+    'define "SDE Ethnicity":\n' +
+    '  SDE."SDE Ethnicity"\n' +
+    '\n' +
+    'define "SDE Payer":\n' +
+    '  SDE."SDE Payer"\n' +
+    '\n' +
+    'define "SDE Race":\n' +
+    '  SDE."SDE Race"\n' +
+    '\n' +
+    'define "SDE Sex":\n' +
+    '  SDE."SDE Sex"\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    'define "Initial Population":\n' +
+    'true\n' +
+    '\n' +
+    'define "Num":\n' +
+    'true\n' +
+    '\n' +
+    'define "Denom":\n' +
+    'true'
 
 describe('Warning modal on Test Case JSON Editor', () => {
 
@@ -603,4 +667,52 @@ describe('JSON Resource ID tests -- CV', () => {
     })
 
 })
+describe('Tests around cardinality violations', () => {
+
+    beforeEach('Create New Measure and Login', () => {
+
+        //Create New Measure
+        CreateMeasurePage.CreateQICoreMeasureAPI(measureName, CqlLibraryName, measureCQLCardTests)
+        OktaLogin.Login()
+        MeasuresPage.measureAction("edit")
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{end} {enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        //wait for alert / successful save message to appear
+        Utilities.waitForElementVisible(CQLEditorPage.successfulCQLSaveNoErrors, 40700)
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+        OktaLogin.Logout()
+        //create Measure Group
+        MeasureGroupPage.CreateProportionMeasureGroupAPI(false, false, 'Initial Population',
+            'Num', 'Initial Population', 'boolean')
+        OktaLogin.Login()
+
+    })
+
+    afterEach('Log out', () => {
+
+        OktaLogin.Logout()
+        //Utilities.deleteMeasure(measureName, CqlLibraryName)
+    })
+    it('Verify error is returned when there is a model violation -- use an object where expecting an array', () => {
+        cy.get(Header.measures).click()
+        MeasuresPage.measureAction("edit")
+
+        //Navigate to Test Cases page and add Test Case details
+        TestCasesPage.createTestCase(testCaseTitle, testCaseDescription, testCaseSeries, cardInvalidTCJson)
+
+        //navigate to test cases' details page
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        //wait for and then click to view the errors
+        Utilities.waitForElementVisible(TestCasesPage.testCaseJsonValidationErrorBtn, 30700)
+        cy.get(TestCasesPage.testCaseJsonValidationErrorBtn).click()
+
+        //wait for and confirm the correct error appears
+        Utilities.waitForElementVisible(TestCasesPage.testCaseJsonValidationDisplayList, 30700)
+        cy.get(TestCasesPage.testCaseJsonValidationDisplayList).should('contain.text', 'HAPI-1820: Found incorrect type for element partOf - Expected ARRAY and found OBJECT')
+
+    })
+})
+
 
