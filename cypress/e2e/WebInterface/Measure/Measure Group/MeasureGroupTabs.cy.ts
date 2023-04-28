92c5253251ab102299d0ab2862fed71034521931
Brandon Welch <brandon.welch@semanticbits.com>
2023-04-28 08:05:49 -0500

    Created some basic automation around some simple validation points on the Scoring field that is located on the new QDM Base Configuration page. Updated support files to accommodate for this, as well.

--- cypress/e2e/WebInterface/Measure/Measure Group/MeasureGroupTabs.cy.ts
+++ cypress/e2e/WebInterface/Measure/Measure Group/MeasureGroupTabs.cy.ts
@@ -1116,7 +1116,7 @@ describe('Supplemental data elements and Risk Adjustment variables on Measure gr
     })
 })
 
-describe('Validating Population tabs specific to QDM', () => {
+describe('Validating Population tabs and fields, specific to QDM', () => {
     beforeEach('Create measure and login', () => {
         let randValue = (Math.floor((Math.random() * 1000) + 1))
         newMeasureName = measureName + randValue
@@ -1124,7 +1124,7 @@ describe('Validating Population tabs specific to QDM', () => {
 
         //Create New Measure
         CreateMeasurePage.CreateQDMMeasureAPI(newMeasureName, newCqlLibraryName, measureCQL)
-        MeasureGroupPage.CreateProportionMeasureGroupAPI(false, false, 'ipp', 'num', 'denom')
+        //MeasureGroupPage.CreateProportionMeasureGroupAPI(false, false, 'ipp', 'num', 'denom')
         OktaLogin.Login()
 
     })
@@ -1139,8 +1139,8 @@ describe('Validating Population tabs specific to QDM', () => {
         Utilities.deleteMeasure(newMeasureName, newCqlLibraryName)
 
     })
-
-    it('Verify that the Base Configuration sub-tab is present', () => {
+    //skipping due to 'Base Configuration' sitting behind the QDM feature flag
+    it.skip('Verify that the Base Configuration sub-tab is present', () => {
 
         MeasuresPage.measureAction("edit")
 
@@ -1151,4 +1151,35 @@ describe('Validating Population tabs specific to QDM', () => {
 
         cy.get(MeasureGroupPage.leftPanelBaseConfigTab).should('be.visible')
     })
+    //this test is work in progress and will need to continue to be built out as the base configuration page becomes more complete
+    //this test will also need to be skipped until the QDM flag is removed
+    it.skip('Verify that the Base Configuration fields are present, contain values when necessary, and, if required, ' +
+        'the save button is not available until all required fields have a value', () => {
+
+            MeasuresPage.measureAction("edit")
+
+            //Click on Measure Group tab
+            Utilities.waitForElementVisible(EditMeasurePage.measureGroupsTab, 30000)
+            cy.get(EditMeasurePage.measureGroupsTab).should('exist')
+            cy.get(EditMeasurePage.measureGroupsTab).click()
+
+            cy.get(MeasureGroupPage.leftPanelBaseConfigTab).should('be.visible')
+            cy.get(MeasureGroupPage.leftPanelBaseConfigTab).click()
+
+            //select scoring unit on measure
+            Utilities.dropdownSelect(MeasureGroupPage.qdmScoring, MeasureGroupPage.qdmScoringCohort)
+            cy.get(MeasureGroupPage.qdmScoring).should('contain.text', 'Cohort')
+
+            //select scoring unit on measure
+            Utilities.dropdownSelect(MeasureGroupPage.qdmScoring, MeasureGroupPage.qdmScoringCV)
+            cy.get(MeasureGroupPage.qdmScoring).should('contain.text', 'Continuous Variable')
+
+            //select scoring unit on measure
+            Utilities.dropdownSelect(MeasureGroupPage.qdmScoring, MeasureGroupPage.qdmScoringRatio)
+            cy.get(MeasureGroupPage.qdmScoring).should('contain.text', 'Ratio')
+
+            //select scoring unit on measure
+            Utilities.dropdownSelect(MeasureGroupPage.qdmScoring, MeasureGroupPage.qdmScoringProportion)
+            cy.get(MeasureGroupPage.qdmScoring).should('contain.text', 'Proportion')
+        })
 })
