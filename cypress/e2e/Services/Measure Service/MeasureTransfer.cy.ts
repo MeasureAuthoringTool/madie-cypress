04e76f34e229dc498e6d335c0e43ea081a924cc1
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2023-06-23 12:13:31 -0400

    MAT-4649

---
+++ cypress/e2e/Services/Measure Service/MeasureTransfer.cy.ts
@@ -0,0 +1,102 @@
+import {MeasureCQL} from "../../../Shared/MeasureCQL"
+import {CreateMeasurePage} from "../../../Shared/CreateMeasurePage"
+import {Utilities} from "../../../Shared/Utilities"
+import {Environment} from "../../../Shared/Environment"
+
+let measureName = 'TestMeasure' + Date.now()
+let cqlLibraryName = 'TestCql' + Date.now()
+let measureSharingAPIKey = Environment.credentials().measureSharing_API_Key
+let harpUserALT = Environment.credentials().harpUserALT
+let measureCQL = MeasureCQL.SBTEST_CQL
+
+describe('Measure Transfer Service', () => {
+
+    beforeEach('Create Measure and Set Access Token', () => {
+
+        cy.setAccessTokenCookie()
+
+        CreateMeasurePage.CreateQICoreMeasureAPI(measureName, cqlLibraryName, measureCQL)
+    })
+
+    afterEach('Clean up', () => {
+
+        Utilities.deleteMeasure(measureName, cqlLibraryName, false, true)
+
+    })
+
+    it('Successful Measure transfer', () => {
+
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((id) => {
+                cy.request({
+                    url: '/api/measures/' + id + '/ownership?userid=' + harpUserALT,
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value,
+                        'api-key': measureSharingAPIKey
+                    },
+                    method: 'PUT'
+
+                }).then((response) => {
+                    expect(response.status).to.eql(200)
+                    expect(response.body).to.eql(harpUserALT + ' granted ownership to Measure successfully.')
+                })
+            })
+        })
+    })
+})
+
+describe('Measure Transfer Validations', () => {
+
+    beforeEach('Create Measure and Set Access Token', () => {
+
+        cy.setAccessTokenCookie()
+
+        CreateMeasurePage.CreateQICoreMeasureAPI(measureName, cqlLibraryName, measureCQL)
+    })
+
+    afterEach('Clean up', () => {
+
+        Utilities.deleteMeasure(measureName, cqlLibraryName)
+
+    })
+
+    it('Verify error message when wrong API key is provided', () => {
+
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((id) => {
+                cy.request({
+                    failOnStatusCode: false,
+                    url: '/api/measures/' + id + '/ownership?userid=' + harpUserALT,
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value,
+                        'api-key': '1233'
+                    },
+                    method: 'PUT'
+                }).then((response) => {
+                    expect(response.status).to.eql(403)
+                })
+            })
+        })
+    })
+
+    it('Verify error message when the Measure does not exist in MADiE', () => {
+
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((id) => {
+                cy.request({
+                    failOnStatusCode: false,
+                    url: '/api/measures/' + id+5 + '/ownership?userid=' + harpUserALT,
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value,
+                        'api-key': measureSharingAPIKey
+                    },
+                    method: 'PUT'
+                }).then((response) => {
+                    expect(response.status).to.eql(400)
+                    expect(response.body).to.eql('Measure does not exist.')
+                })
+            })
+        })
+    })
+})
+
