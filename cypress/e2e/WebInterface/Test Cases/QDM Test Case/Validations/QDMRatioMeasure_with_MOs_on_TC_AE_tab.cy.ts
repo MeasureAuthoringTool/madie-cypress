91447f4a07f6e8211f63736f95310270a7c002aa
Brandon Welch <brandon.welch@semanticbits.com>
2023-06-30 10:31:17 -0500

    Updated preveious test to increase coverage to include saving test case and validating that observation values remain. Also, added an additional test to cover non-owner availability of the observation fields.

--- cypress/e2e/WebInterface/Test Cases/QDM Test Case/Validations/QDMRatioMeasure_with_MOs_on_TC_AE_tab.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/QDM Test Case/Validations/QDMRatioMeasure_with_MOs_on_TC_AE_tab.cy.ts
@@ -21,7 +21,7 @@ let testCaseJson = TestCaseJson.QDMTestCaseJson
 
 describe('Measure Creation: Ratio ListQDMPositiveEncounterPerformed with MO', () => {
 
-    before('Create Measure', () => {
+    beforeEach('Create Measure', () => {
 
         //Create New Measure
         CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(measureName, CqlLibraryName, scoringValue, false, measureCQL, false, false,
@@ -46,7 +46,7 @@ describe('Measure Creation: Ratio ListQDMPositiveEncounterPerformed with MO', ()
         OktaLogin.Logout()
     })
 
-    after('Clean up', () => {
+    afterEach('Clean up', () => {
 
         OktaLogin.Logout()
 
@@ -119,5 +119,136 @@ describe('Measure Creation: Ratio ListQDMPositiveEncounterPerformed with MO', ()
         //if one is entered in the numerator exclusion field then one of the numerator observation fields will be removed
         cy.get(TestCasesPage.testCaseNUMEXExpected).type('1')
         cy.get(TestCasesPage.numer2Observation).should('not.exist')
+
+        //save test case
+        cy.get(TestCasesPage.editTestCaseSaveButton).click()
+        cy.get(TestCasesPage.importTestCaseSuccessMsg).should('contain.text', 'Test Case Updated Successfully')
+
+        //navigate to the main measures page and edit the measure
+        cy.get(Header.measures).click()
+        MeasuresPage.measureAction("edit")
+
+        //navigate to the test case and the E/A sub-tab
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        //Navigate to Edit Test Case page
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        //click on the Expected / Actual tab
+        cy.get(TestCasesPage.expectedOrActualTab).click()
+
+        //validate values that were saved in the E/A fields retain values
+        cy.get(TestCasesPage.testCaseDENOMExpected).should('contain.value', 3)
+        cy.get(TestCasesPage.testCaseDENEXExpected).should('contain.value', 1)
+        cy.get(TestCasesPage.denom0Observation).should('be.visible')
+        cy.get(TestCasesPage.denom1Observation).should('be.visible')
+        cy.get(TestCasesPage.denom2Observation).should('not.exist')
+
+        cy.get(TestCasesPage.testCaseNUMERExpected).should('contain.value', 3)
+        cy.get(TestCasesPage.testCaseNUMEXExpected).should('contain.value', 1)
+        cy.get(TestCasesPage.numer0Observation).should('be.visible')
+        cy.get(TestCasesPage.numer1Observation).should('be.visible')
+        cy.get(TestCasesPage.numer2Observation).should('not.exist')
+    })
+    it('Non-owner of measure cannot eidt observation fields', () => {
+
+        //navigate to the main measures page and edit the measure
+        cy.get(Header.measures).click()
+        MeasuresPage.measureAction("edit")
+
+        //fill out group details
+        cy.get(EditMeasurePage.measureGroupsTab).click()
+        cy.get(MeasureGroupPage.QDMPopulationCriteria1).click()
+        cy.get(MeasureGroupPage.initialPopulationSelect).click()
+        cy.get(MeasureGroupPage.measurePopulationOption).eq(2).click()
+        cy.get(MeasureGroupPage.denominatorSelect).click()
+        cy.get(MeasureGroupPage.measurePopulationOption).eq(2).click()
+        cy.get(MeasureGroupPage.addDenominatorObservationLink).click()
+        cy.get(MeasureGroupPage.denominatorObservation).click()
+        cy.get(MeasureGroupPage.measureObservationSelect).eq(4).click()
+        cy.get(MeasureGroupPage.denominatorAggregateFunction).click()
+        cy.get(MeasureGroupPage.aggregateFunctionDropdownList).eq(1).click()
+        cy.get(MeasureGroupPage.denominatorExclusionSelect).click()
+        cy.get(MeasureGroupPage.measurePopulationOption).eq(2).click()
+        cy.get(MeasureGroupPage.numeratorSelect).click()
+        cy.get(MeasureGroupPage.measurePopulationOption).eq(2).click()
+        cy.get(MeasureGroupPage.addNumeratorObservationLink).click()
+        cy.get(MeasureGroupPage.numeratorObservation).click()
+        cy.get(MeasureGroupPage.measureObservationSelect).eq(4).click()
+        cy.get(MeasureGroupPage.numeratorAggregateFunction).click()
+        cy.get(MeasureGroupPage.aggregateFunctionDropdownList).eq(1).click()
+        cy.get(MeasureGroupPage.numeratorExclusionSelect).click()
+        cy.get(MeasureGroupPage.measurePopulationOption).eq(2).click()
+
+        //save group details
+        cy.get(MeasureGroupPage.saveMeasureGroupDetails).click()
+
+        //confirm details were saved
+        //validation successful save message
+        cy.get(MeasureGroupPage.successfulSaveMeasureGroupMsg).should('exist')
+        cy.get(MeasureGroupPage.successfulSaveMeasureGroupMsg).should('contain.text', 'Population details for this group saved successfully.')
+
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        //Navigate to Edit Test Case page
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        //click on the Expected / Actual tab
+        cy.get(TestCasesPage.expectedOrActualTab).click()
+
+        //enter 3 for the expected denominator to generate 3 observation field
+        cy.get(TestCasesPage.testCaseDENOMExpected).type('3')
+        cy.get(TestCasesPage.denom0Observation).should('be.visible')
+        cy.get(TestCasesPage.denom1Observation).should('be.visible')
+        cy.get(TestCasesPage.denom2Observation).should('be.visible')
+
+        //if one is entered in the denominator exclusion field then one of the denominator observation fields will be removed
+        cy.get(TestCasesPage.testCaseDENEXExpected).type('1')
+        cy.get(TestCasesPage.denom2Observation).should('not.exist')
+
+        //enter 3 for the expected denominator to generate 3 observation field
+        cy.get(TestCasesPage.testCaseNUMERExpected).type('3')
+        cy.get(TestCasesPage.numer0Observation).should('be.visible')
+        cy.get(TestCasesPage.numer1Observation).should('be.visible')
+        cy.get(TestCasesPage.numer2Observation).should('be.visible')
+
+        //if one is entered in the numerator exclusion field then one of the numerator observation fields will be removed
+        cy.get(TestCasesPage.testCaseNUMEXExpected).type('1')
+        cy.get(TestCasesPage.numer2Observation).should('not.exist')
+
+        //save test case
+        cy.get(TestCasesPage.editTestCaseSaveButton).click()
+        cy.get(TestCasesPage.importTestCaseSuccessMsg).should('contain.text', 'Test Case Updated Successfully')
+
+        //log out of MADiE
+        OktaLogin.Logout()
+
+        //log into MADiE as a different user
+        OktaLogin.AltLogin()
+        cy.get(MeasuresPage.allMeasuresTab).click()
+
+        //edit the measure
+        MeasuresPage.measureAction("edit")
+
+        //click on the test case main tab
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        //Navigate to Edit Test Case page
+        TestCasesPage.clickEditforCreatedTestCase()
+
+        //click on the Expected / Actual tab
+        cy.get(TestCasesPage.expectedOrActualTab).click()
+
+        //observation fields cannot be edited
+        cy.get(TestCasesPage.testCaseDENOMExpected).should('not.be.enabled')
+        cy.get(TestCasesPage.denom0Observation).should('not.be.enabled')
+        cy.get(TestCasesPage.denom1Observation).should('not.be.enabled')
+        cy.get(TestCasesPage.testCaseDENEXExpected).should('not.be.enabled')
+
+        cy.get(TestCasesPage.testCaseNUMERExpected).should('not.be.enabled')
+        cy.get(TestCasesPage.numer0Observation).should('not.be.enabled')
+        cy.get(TestCasesPage.numer1Observation).should('not.be.enabled')
+        cy.get(TestCasesPage.testCaseNUMEXExpected).should('not.be.enabled')
+
     })
 })
