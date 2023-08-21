419a3a6b5dd595276cf2ed967da254fb587bf0fc
Brandon <97704721+bwelch8105@users.noreply.github.com>
2023-08-20 20:16:13 -0500

    Added necessary Libraries for QDM in the TEST (#989)
    environment and updated CQL in tests to account for these updates.

--- cypress/e2e/WebInterface/Smoke Tests/QDM  End To End Measure/Ratio_ListQDMPositiveEncounterPerformed_WithMO.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/QDM  End To End Measure/Ratio_ListQDMPositiveEncounterPerformed_WithMO.cy.ts
@@ -4,7 +4,7 @@ import { Utilities } from "../../../../Shared/Utilities"
 import { EditMeasurePage } from "../../../../Shared/EditMeasurePage"
 import { MeasuresPage } from "../../../../Shared/MeasuresPage"
 import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
-import {MeasureGroupPage} from "../../../../Shared/MeasureGroupPage";
+import { MeasureGroupPage } from "../../../../Shared/MeasureGroupPage";
 
 let measureName = 'RatioListQDMPositiveEncounterPerformedWithMO' + Date.now()
 let CqlLibraryName = 'RatioListQDMPositiveEncounterPerformedWithMO' + Date.now()
