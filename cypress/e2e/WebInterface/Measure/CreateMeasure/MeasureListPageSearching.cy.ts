96d1e00a00f0452c2520d869e30fab2ed3a55f4a
praveenavas <82400011+praveenavas@users.noreply.github.com>
2023-08-24 11:51:54 -0400

    Fix for MeasureListPageSearching.cy (#1003)
    * Fix for regression failures
    
    * Fix for TestCaseImport.cy.ts
    
    * Fix for ExecuteTestCasesByNonMeasureOwner.cy
    
    * Fix for DeleteTest-Case.cy
    
    * Fix for MeasureListPageSearching.cy

--- cypress/e2e/WebInterface/Measure/CreateMeasure/MeasureListPageSearching.cy.ts
+++ cypress/e2e/WebInterface/Measure/CreateMeasure/MeasureListPageSearching.cy.ts
@@ -27,19 +27,19 @@ describe('Measure List Page Searching', () => {
         //Search for the Measure using Measure name
         cy.log('Search Measure with measure name')
         cy.get(MeasuresPage.searchInputBox).type(measureName).wait(1000).type('{enter}')
-        cy.get(MeasuresPage.measureListTitles).should('contain', measureName)
+        cy.get('[data-testid="row-item"] > :nth-child(1)').should('contain', measureName)
 
         //Search for the Measure under All Measures tab
         cy.log('Search Measure with measure name under All Measures tab')
         cy.get(MeasuresPage.allMeasuresTab).wait(1000).click()
         cy.get(MeasuresPage.searchInputBox).clear().type(measureName).wait(1000).type('{enter}')
-        cy.get(MeasuresPage.measureListTitles).should('contain', measureName)
+        cy.get('[data-testid="row-item"] > :nth-child(1)').should('contain', measureName)
 
         //Search for the Measure using eCQMTitle
         cy.log('Search Measure with eCQM Title')
         cy.get(LandingPage.myMeasuresTab).wait(1000).click()
         cy.get(MeasuresPage.searchInputBox).clear().type('eCQMTitle01').wait(1000).type('{enter}')
-        cy.get(MeasuresPage.measureListTitles).should('contain', measureName)
+        cy.get('[data-testid="row-item"] > :nth-child(1)').should('contain', measureName)
         MeasuresPage.measureAction("edit")
         cy.get(CreateMeasurePage.eCQMAbbreviatedTitleTextbox).should('have.value', 'eCQMTitle01')
 
@@ -53,13 +53,14 @@ describe('Measure List Page Searching', () => {
         //Search for Deleted Measure
         cy.log('Search for Deleted Measure')
         cy.get(MeasuresPage.searchInputBox).type(measureName).wait(1000).type('{enter}')
-        cy.get(MeasuresPage.measureListTitles).should('not.exist')
+        cy.get('[data-testid="row-item"] > :nth-child(1)').should('not.exist')
 
         //Search for deleted Measure under All Measures tab
         cy.log('Search for Deleted Measure under All Measures tab')
         cy.get(MeasuresPage.allMeasuresTab).wait(1000).click()
+        cy.reload()
         cy.get(MeasuresPage.searchInputBox).clear().type(measureName).wait(1000).type('{enter}')
-        cy.get(MeasuresPage.measureListTitles).should('not.exist')
+        cy.get('[data-testid="row-item"] > :nth-child(1)').should('not.exist')
 
     })
 
