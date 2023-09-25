54a2a8524dd63f48e2f4b30a9fa22065c03620d0
praveena.vasireddy <praveena.vasireddy@semanticbits.com>
2023-09-25 14:50:14 -0400

    Fix for DefaultValuesOnTestCaseImport.cy

--- cypress/e2e/WebInterface/Test Cases/TestCaseExportImport/DefaultValuesOnTestCaseImport.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/TestCaseExportImport/DefaultValuesOnTestCaseImport.cy.ts
@@ -15,8 +15,8 @@ let fileToUpload = ['PatientFilesForJsonImport/NumFail_MedAdminStatus.json', 'Pa
     'PatientFilesForJsonImport/MedicationRequest_WithNullStatus.json', 'PatientFilesForJsonImport/IPFail_ExpiredCoverage.json', 'PatientFilesForJsonImport/Patient_WithCondition.json',
     'PatientFilesForJsonImport/Patient_WithObservation.json', 'PatientFilesForJsonImport/Patient_WithProcedure.json', 'PatientFilesForJsonImport/Patient_withNullProcedure.status.json',
     'PatientFilesForJsonImport/Patient_WithServiceRequest.json', 'PatientFilesForJsonImport/Patient_WithNullEncounterStatus.json']
-let ValueToBeAdded = ['subject', 'subject', '"status" : "active"', '"intent" : "order"', '"status" : "active"', '"intent" : "order"', '"status" : "active"',
-    'beneficiary', 'subject', 'subject', 'subject', '"status" : "completed"', 'subject', '"status" : "finished"']
+let ValueToBeAdded = ['subject', 'subject', '"status": "active"', '"intent": "order"', '"status": "active"', '"intent": "order"', '"status": "active"',
+    'beneficiary', 'subject', 'subject', 'subject', '"status": "completed"', 'subject', '"status": "finished"']
 let Resource = ['Medication Administration Status', 'Medication Request Status', 'Service Request With Status', 'Service Request With Intent',
     'Coverage Status', 'MedicationRequestWithIntent', 'MedicationRequestWithStatus', 'Coverage', 'Condition', 'Observation', 'Procedure',
     'Procedure With Status', 'Service Request', 'Encounter With Status']
