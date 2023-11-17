75a03234e1b0e12cf1472e390abf4b145b532b15
praveenavas <82400011+praveenavas@users.noreply.github.com>
2023-11-17 13:10:44 -0500

    MAT-6434 Added special characters to the fields (#1091)
    * Fix for Smoke test failures
    
    * Fix for Regression failures
    
    * Fix for DeleteTest-Case.cy ans CQLLibraryTransfer.cy
    
    * MAT-5688 Import QDM Test cases from Bonnie
    
    * MAT-5688 Json Files
    
    * MAT-6434 Added special characters to the fields
    
    * MAT-6388 Related to Attribute

--- cypress/e2e/WebInterface/Smoke Tests/Export/ProportionPatientQicoreProfileTypes.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/Export/ProportionPatientQicoreProfileTypes.cy.ts
@@ -5,6 +5,7 @@ import { MeasuresPage } from "../../../../Shared/MeasuresPage"
 import { MeasureGroupPage } from "../../../../Shared/MeasureGroupPage"
 import { EditMeasurePage } from "../../../../Shared/EditMeasurePage"
 import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
+import {Header} from "../../../../Shared/Header"
 
 let measureNameTimeStamp = 'TestMeasure' + Date.now()
 let measureName = measureNameTimeStamp
@@ -220,6 +221,64 @@ describe.skip('FHIR Measure Export for Proportion Patient Measure with QI-Core P
 
     it('Validate the zip file Export is downloaded and can be unzipped', () => {
 
+        MeasuresPage.measureAction('edit')
+
+        //Description
+        cy.get(EditMeasurePage.leftPanelDescription).click()
+        cy.get(EditMeasurePage.measureDescriptionTextBox).clear().type('Percentage of cataract surgeries for patients aged 18 and older with a diagnosis of uncomplicated cataract and no significant ocular conditions impacting the visual outcome of surgery and had best-corrected visual acuity of 20/40 or better (distance or near) achieved in the operative eye within 90 days following the cataract surgery')
+        cy.get(EditMeasurePage.measureDescriptionSaveButton).click()
+        cy.get(EditMeasurePage.measureDescriptionSuccessMessage).should('be.visible')
+
+        //Copyright
+        cy.get(EditMeasurePage.leftPanelCopyright).click()
+        cy.get(EditMeasurePage.measureCopyrightTextBox).clear().type('Test!@#$%^&*()_+-={}|`~[]\:"<>?;\',./~`')
+        cy.get(EditMeasurePage.measureCopyrightSaveButton).click()
+        cy.get(EditMeasurePage.measureCopyrightSuccessMessage).should('be.visible')
+
+        //Disclaimer
+        cy.get(EditMeasurePage.leftPanelDisclaimer).click()
+        cy.get(EditMeasurePage.measureDisclaimerTextBox).clear().type('Test!@#$%^&*()_+-={}|`~[]\:"<>?;\',./~`')
+        cy.get(EditMeasurePage.measureDisclaimerSaveButton).click()
+        cy.get(EditMeasurePage.measureDisclaimerSuccessMessage).should('be.visible')
+
+        //Rationale
+        cy.get(EditMeasurePage.leftPanelRationale).click()
+        cy.get(EditMeasurePage.measureRationaleTextBox).clear().type('Test!@#$%^&*()_+-={}|`~[]\:"<>?;\',./~`')
+        cy.get(EditMeasurePage.measureRationaleSaveButton).click()
+        cy.get(EditMeasurePage.measureRationaleSuccessMessage).should('be.visible')
+
+        //Steward & Developers
+        cy.get(EditMeasurePage.leftPanelStewardDevelopers).click()
+        //select a value for Steward
+        cy.get(EditMeasurePage.measureStewardDrpDwn).should('exist').should('be.visible').click().type('Able Health')
+        cy.get(EditMeasurePage.measureStewardDrpDwnOption).click()
+
+        //select a value for Developers
+        cy.get(EditMeasurePage.measureDeveloperDrpDwn).should('exist').should('be.visible').click().type("ACO Health Solutions")
+        cy.get(EditMeasurePage.measureDevelopersDrpDwnOption).click()
+        cy.get(EditMeasurePage.measureStewardDevelopersSaveButton).should('exist')
+        cy.get(EditMeasurePage.measureStewardDevelopersSaveButton).should('be.visible')
+        //save button should become available, now, because a value is, now, in both fields
+        cy.get(EditMeasurePage.measureStewardDevelopersSaveButton).should('be.enabled')
+
+        //save Steward & Developers
+        cy.get(EditMeasurePage.measureStewardDevelopersSaveButton).click({ force: true })
+        cy.get(EditMeasurePage.measureStewardDevelopersSuccessMessage).should('be.visible')
+
+        //Guidance
+        cy.get(EditMeasurePage.leftPanelGuidance).click()
+        cy.get(EditMeasurePage.measureGuidanceTextBox).clear().type('Test!@#$%^&*()_+-={}|`~[]\:"<>?;\',./~`')
+        cy.get(EditMeasurePage.measureGuidanceSaveButton).click()
+        cy.get(EditMeasurePage.measureGuidanceSuccessMessage).should('be.visible')
+
+        //Clinical Recommendation
+        cy.get(EditMeasurePage.leftPanelMClinicalGuidanceRecommendation).click()
+        cy.get(EditMeasurePage.measureClinicalRecommendationTextBox).clear().type('Test!@#$%^&*()_+-={}|`~[]\:"<>?;\',./~`')
+        cy.get(EditMeasurePage.measureClinicalRecommendationSaveButton).click()
+        cy.get(EditMeasurePage.measureClinicalRecommendationSuccessMessage).should('be.visible')
+
+        cy.get(Header.measures).click().wait(1000)
+
         MeasuresPage.measureAction('version')
 
         cy.get(MeasuresPage.measureVersionMajor).should('exist')
@@ -239,7 +298,6 @@ describe.skip('FHIR Measure Export for Proportion Patient Measure with QI-Core P
         cy.log('Successfully verified zip file export')
         console.log('Successfully verified zip file export')
 
-        //cy.get(MeasuresPage.exportFinishedContinueBtn).click()
         OktaLogin.Logout()
 
     })
