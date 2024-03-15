6a80a9292a10b4453c8f04b9a7ba26e5f5bf5774
praveenavas <prveena.vasireddy@icf.com>
2024-03-15 12:06:25 -0400

    MAT-6663 SDE sub tab

--- cypress/e2e/WebInterface/Test Cases/QDM Test Case/QDM Test Case Validations/QDMSDESubTabValidations.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/QDM Test Case/QDM Test Case Validations/QDMSDESubTabValidations.cy.ts
@@ -31,7 +31,6 @@ describe.skip( 'QDM Test Cases : SDE Sub tab validations', () => {
     afterEach('Log out and Clean up', () => {
 
         OktaLogin.Logout()
-
         Utilities.deleteMeasure(measureName, CqlLibraryName)
 
     })
@@ -185,6 +184,122 @@ describe.skip( 'QDM Test Cases : SDE Sub tab validations', () => {
         cy.get(TestCasesPage.tcHighlightingTab).click()
         //Click on SDE Sub tab
         cy.get(TestCasesPage.qdmSDESubTab).should('not.exist')
+
+    })
+
+    it('Test Case Coverage Percentage updated based on the SDE selection', () => {
+
+        //Click on Edit Button
+        MeasuresPage.measureAction("edit")
+
+        //Save CQL
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('contain.text', 'CQL updated successfully! Library Statement or Using Statement were incorrect. MADiE has overwritten them to ensure proper CQL.')
+
+        //Click on Measure Group tab
+        Utilities.waitForElementVisible(EditMeasurePage.measureGroupsTab, 30000)
+        cy.get(EditMeasurePage.measureGroupsTab).should('exist')
+        cy.get(EditMeasurePage.measureGroupsTab).click()
+
+        //Add Supplemental Data Elements
+        cy.get(MeasureGroupPage.leftPanelSupplementalDataTab).click()
+        cy.get(MeasureGroupPage.supplementalDataDefinitionSelect).click()
+        cy.get(MeasureGroupPage.supplementalDataDefinitionDropdown).contains('SDE Ethnicity').click()
+        cy.get(MeasureGroupPage.supplementalDataDefinitionDropdown).contains('SDE Payer').click()
+        cy.get(MeasureGroupPage.supplementalDataDefinitionDropdown).contains('SDE Race').click()
+        cy.get(MeasureGroupPage.supplementalDataDefinitionDropdown).scrollIntoView().contains('SDE Sex').click()
+
+        //Save Supplemental data
+        cy.get('[data-testid="measure-Supplemental Data-save"]').click({ force: true })
+        cy.get(MeasureGroupPage.supplementalDataElementsSaveSuccessMsg).should('contain.text', 'Measure Supplemental Data have been Saved Successfully')
+
+        //Navigate to test case page
+        cy.get(EditMeasurePage.testCasesTab).should('be.visible')
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        //Navigate to Edit Test Case page
+        TestCasesPage.testCaseAction('edit')
+
+        //enter a value of the dob, Race and gender
+        cy.get(TestCasesPage.QDMDob).type('01/01/2020').click()
+        cy.get(TestCasesPage.QDMLivingStatus).click()
+        cy.get(TestCasesPage.QDMLivingStatusOPtion).contains('Expired').click()
+        cy.get(TestCasesPage.QDMRace).click()
+        cy.get(TestCasesPage.QDMRaceOption).contains('White').click()
+        cy.get(TestCasesPage.QDMGender).click()
+        cy.get(TestCasesPage.QDMGenderOption).contains('Male').click()
+        cy.get(TestCasesPage.QDMEthnicity).click()
+        cy.get(TestCasesPage.QEMEthnicityOptions).contains('Not Hispanic or Latino').click()
+
+        //Selecting Laboratory element and performed
+        cy.get(TestCasesPage.laboratoryElement).click()
+        cy.get(TestCasesPage.plusIcon).eq(0).click()
+
+        //navigating to the attribute sub-tab
+        cy.get(TestCasesPage.attributesTab).click()
+
+        //selecting attribute
+        cy.get(TestCasesPage.selectAttributeDropdown).click()
+        cy.get(TestCasesPage.referenceRangeAttribute).click()
+        cy.get(TestCasesPage.attributeType).should('contain.text', 'Interval<Quantity>')
+
+        //adding a date value tot he attribute
+        cy.get('[id="dateTime"]').eq(0).type('09/12/2023 12:00 AM')
+
+        //adding values to the attribute
+        cy.get('[data-testid="quantity-value-input-low"]').type('2')
+        cy.get('[id="quantity-unit-input-low"]').click()
+        cy.get('[id="quantity-unit-input-low"]').type('m') //Select unit as m meter
+        cy.get('[data-testid=quantity-value-input-high]').type('4')
+        cy.get('[id="quantity-unit-input-high"]').click()
+        cy.get('[id="quantity-unit-input-high"]').type('m') //Select unit as m meter
+        cy.get(TestCasesPage.addAttribute).click() //click the "Add" button
+
+        //save the Test Case
+        cy.get(TestCasesPage.QDMTCSaveBtn).should('be.enabled')
+        cy.get(TestCasesPage.QDMTCSaveBtn).click()
+        cy.get(TestCasesPage.tcSaveSuccessMsg).should('contain.text', 'Test Case Updated Successfully')
+
+        //Navigate to test case page
+        cy.get(EditMeasurePage.testCasesTab).should('be.visible')
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        //Execute test case
+        cy.get(TestCasesPage.executeTestCaseButton).should('be.visible')
+        cy.get(TestCasesPage.executeTestCaseButton).should('be.enabled').wait(1000)
+        cy.get(TestCasesPage.executeTestCaseButton).click()
+
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('exist')
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('be.visible')
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('contain.text', '0%')
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('contain.text', 'Coverage')
+
+        //Navigate to the SDE side tab and select Yes
+        Utilities.waitForElementVisible(TestCasesPage.qdmSDESidNavLink, 30000)
+        cy.get(TestCasesPage.qdmSDESidNavLink).click()
+
+        cy.get(TestCasesPage.includeSDERadioBtn).eq(0).click()
+        cy.get(TestCasesPage.saveSDEOption).click()
+        cy.get(TestCasesPage.tcSaveSuccessMsg).should('contain.text', 'Test Case Configuration Updated Successfully')
+
+        //Navigate to test case page
+        cy.get(EditMeasurePage.testCasesTab).should('be.visible')
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        cy.reload()
+
+        //Execute test case
+        cy.get(TestCasesPage.executeTestCaseButton).should('be.visible')
+        cy.get(TestCasesPage.executeTestCaseButton).should('be.enabled').wait(1000)
+        cy.get(TestCasesPage.executeTestCaseButton).click()
+
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('exist')
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('be.visible')
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('contain.text', '6%')
+        cy.get(TestCasesPage.testCaseListCoveragePercTab).should('contain.text', 'Coverage')
 
     })
 })
