bfc0365f480c9d7bd89c9aa7acc40c788928ecd1
bwelch8105 <brandon.welch@semanticbits.com>
2024-03-25 10:34:12 -0500

    Added test to confirm / verify messaging around when QDM Test Case has speical characters in the series and title values.

--- cypress/e2e/WebInterface/Test Cases/QDM Test Case/QDM Test Case Validations/QRDATestCaseExport.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/QDM Test Case/QDM Test Case Validations/QRDATestCaseExport.cy.ts
@@ -1,12 +1,12 @@
-import {MeasureCQL} from "../../../../../Shared/MeasureCQL"
-import {CreateMeasurePage} from "../../../../../Shared/CreateMeasurePage"
-import {MeasureGroupPage} from "../../../../../Shared/MeasureGroupPage"
-import {TestCasesPage} from "../../../../../Shared/TestCasesPage"
-import {OktaLogin} from "../../../../../Shared/OktaLogin"
-import {Utilities} from "../../../../../Shared/Utilities"
-import {MeasuresPage} from "../../../../../Shared/MeasuresPage"
-import {EditMeasurePage} from "../../../../../Shared/EditMeasurePage"
-import {CQLEditorPage} from "../../../../../Shared/CQLEditorPage"
+import { MeasureCQL } from "../../../../../Shared/MeasureCQL"
+import { CreateMeasurePage } from "../../../../../Shared/CreateMeasurePage"
+import { MeasureGroupPage } from "../../../../../Shared/MeasureGroupPage"
+import { TestCasesPage } from "../../../../../Shared/TestCasesPage"
+import { OktaLogin } from "../../../../../Shared/OktaLogin"
+import { Utilities } from "../../../../../Shared/Utilities"
+import { MeasuresPage } from "../../../../../Shared/MeasuresPage"
+import { EditMeasurePage } from "../../../../../Shared/EditMeasurePage"
+import { CQLEditorPage } from "../../../../../Shared/CQLEditorPage"
 
 let qdmMeasureCQL = MeasureCQL.CQLQDMObservationRun
 let measureName = 'QDMTestMeasure' + Date.now()
@@ -14,12 +14,17 @@ let CqlLibraryName = 'QDMCQLLibrary' + Date.now()
 let firstTestCaseTitle = 'PDxNotPsych60MinsDepart'
 let testCaseDescription = 'IPPStrat1Pass' + Date.now()
 let testCaseSeries = 'SBTestSeries'
+
+let SecondTestCaseTitle = 'PDxNotPsych60MinsDepart(){}[]<>/|"\':;,.~`!@#$%^&*_+='
+let SecondtestCaseDescription = 'IPPStrat1Pass' + Date.now()
+let SecondtestCaseSeries = 'SBTestSeries(){}[]<>/|"\':;,.~`!@#$%^&*_+='
+
 const path = require('path')
 const downloadsFolder = Cypress.config('downloadsFolder')
 const { deleteDownloadsFolderBeforeAll } = require('cypress-delete-downloads-folder')
 
 // skipping until 'TestCaseExport' flag is removed
-describe.skip( 'QDM Test Cases : Export Test Case', () => {
+describe.skip('QDM Test Cases : Export Test Case', () => {
 
     deleteDownloadsFolderBeforeAll()
 
@@ -92,6 +97,67 @@ describe.skip( 'QDM Test Cases : Export Test Case', () => {
 
         //Export test case button should be enabled
         cy.get(TestCasesPage.testcaseQRDAExportBtn).should('be.enabled')
+    })
+})
+
+// skipping until 'TestCaseExport' flag is removed
+// tests ensuring that invalid characters in, either, the group or title of the test case prevents test case export
+describe.skip('QDM Test Cases : Export Test Case: Invalid / Special characters in the series or in title prevents export', () => {
+
+    deleteDownloadsFolderBeforeAll()
+
+    beforeEach('Create Measure, Measure Group, Test cases and Log in', () => {
+
+        //Create New Measure and Test case
+        CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(measureName, CqlLibraryName, 'Cohort', false, qdmMeasureCQL)
+        MeasureGroupPage.CreateCohortMeasureGroupAPI(false, false, 'Initial Population')
+        TestCasesPage.CreateQDMTestCaseAPI(firstTestCaseTitle, testCaseSeries, testCaseDescription)
+        TestCasesPage.CreateQDMTestCaseAPI(SecondTestCaseTitle, SecondtestCaseSeries, SecondtestCaseDescription, null, true)
+
+        OktaLogin.Login()
+    })
+
+    afterEach('Log out and Clean up', () => {
+
+        OktaLogin.Logout()
+
+        Utilities.deleteMeasure(measureName, CqlLibraryName)
+
+    })
+
+    it('Attempt to generate QRDA Export, for QDM Test Cases, when special characters are in one of the test case\'s title and series values', () => {
+
+        //Click on Edit Button
+        MeasuresPage.measureAction("edit")
+
+        //Save CQL
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('contain.text', 'CQL updated successfully! Library Statement or Using Statement were incorrect. MADiE has overwritten them to ensure proper CQL.')
+
+        //Navigate to test case page
+        cy.get(EditMeasurePage.testCasesTab).should('be.visible')
+        cy.get(EditMeasurePage.testCasesTab).click()
+
+        //Run the Test cases
+        cy.get(TestCasesPage.executeTestCaseButton).click()
+
+        cy.get(TestCasesPage.testcaseQRDAExportBtn).click()
+        cy.get('[class="btn-container"]').contains('QRDA').click()
+
+        cy.get(MeasureGroupPage.CQLPCMismatchError)
+            .find('[id="content"]')
+            .find('[data-testid="execution_context_loading_errors"]')
+            .find('[data-testid="error-special-char-title"]')
+            .should('contain.text', 'Test Cases can not be exported some titles or groups contain special characters.')
+
+        cy.get(MeasureGroupPage.CQLPCMismatchError)
+            .find('[id="content"]')
+            .find('[data-testid="execution_context_loading_errors"]')
+            .find('[data-testid="error-special-char"]')
+            .should('contain.text', 'SBTestSeries(){}[]<>/|"\':;,.~`!@#$%^&*_+= PDxNotPsych60MinsDepart(){}[]<>/|"\':;,.~`!@#$%^&*_+=')
     })
 })
 
