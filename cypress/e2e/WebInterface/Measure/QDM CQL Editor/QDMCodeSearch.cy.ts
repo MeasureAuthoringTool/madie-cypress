b267e75a1cf622c8748634326b66f18ed014e939
praveenavas <prveena.vasireddy@icf.com>
2024-05-09 10:19:05 -0400

    MAT-6853 Apply code to CQL

--- cypress/e2e/WebInterface/Measure/QDM CQL Editor/QDMCodeSearch.cy.ts
+++ cypress/e2e/WebInterface/Measure/QDM CQL Editor/QDMCodeSearch.cy.ts
@@ -13,7 +13,7 @@ let measureCQL = MeasureCQL.qdmCQLNonPatienBasedTest
 //Skipping until QDMCodeSearch feature flag is removed
 describe.skip('QDM Code Search fields', () => {
 
-    before('Create Measure and Login', () => {
+    beforeEach('Create Measure and Login', () => {
 
         //Create New Measure
         CreateMeasurePage.CreateQDMMeasureAPI(measureName, CqlLibraryName, measureCQL, false, false,
@@ -22,14 +22,14 @@ describe.skip('QDM Code Search fields', () => {
         OktaLogin.Login()
     })
 
-    after('Clean up and Logout', () => {
+    afterEach('Clean up and Logout', () => {
 
         OktaLogin.Logout()
         Utilities.deleteMeasure(measureName, CqlLibraryName)
 
     })
 
-    it('Search for the Codes and apply it to Measure', () => {
+    it('Search for the Codes', () => {
 
         //Click on Edit Button
         MeasuresPage.measureAction("edit")
@@ -55,18 +55,20 @@ describe.skip('QDM Code Search fields', () => {
         //Assert when the Code is Active in VSAC
         cy.get(CQLEditorPage.codeSystemDropdown).type('SNOMEDCT')
         cy.get(CQLEditorPage.codeSystemOptionListBox).contains('SNOMEDCT').click()
+        cy.get(CQLEditorPage.codeSystemVersionDropdown).click()
+        cy.get('[data-testid="http://snomed.info/sct/731000124108/version/20230301-option"]').click()
         cy.get(CQLEditorPage.codeText).type('258219007')
         Utilities.waitForElementEnabled(CQLEditorPage.codeSystemSearchBtn, 30000)
         cy.get(CQLEditorPage.codeSystemSearchBtn).click()
         Utilities.waitForElementVisible(CQLEditorPage.codeSystemSearchResultsTbl, 30000)
         cy.get(CQLEditorPage.toolTip).trigger('mouseover')
         cy.get(CQLEditorPage.toolTipMsg).should('contain.text', 'This code is active in this code system version')
-        cy.get(CQLEditorPage.codeSystemSearchResultsTbl).should('contain.text', 'CodeDescriptionCode SystemSystem Version258219007Stage 2 (qualifier value)SNOMEDCThttp://snomed.info/sct/731000124108/version/20230901')
+        cy.get(CQLEditorPage.codeSystemSearchResultsTbl).should('contain.text', 'CodeDescriptionCode SystemSystem Version258219007Stage 2 (qualifier value)SNOMEDCThttp://snomed.info/sct/731000124108/version/20230301Select')
 
         //Assert when the Code is not available in VSAC
         cy.get(CQLEditorPage.codeText).clear().type('123')
         cy.get(CQLEditorPage.codeSystemSearchBtn).click()
-        cy.get('[class="sc-dhKdcB bOvcnM"]').should('contain.text', 'No Results were found')
+        cy.get('[class="sc-kpDqfm fBkgsf"]').should('contain.text', 'No Results were found')
 
         //Clear the code search values
         cy.get(CQLEditorPage.clearCodeBtn).click()
@@ -74,14 +76,15 @@ describe.skip('QDM Code Search fields', () => {
         //Assert when the Code is inactive in VSAC
         cy.get(CQLEditorPage.codeSystemDropdown).type('SNOMEDCT')
         cy.get(CQLEditorPage.codeSystemOptionListBox).contains('SNOMEDCT').click()
+        cy.get(CQLEditorPage.codeSystemVersionDropdown).click()
+        cy.get('[data-testid="http://snomed.info/sct/731000124108/version/20230301-option"]').click()
         cy.get(CQLEditorPage.codeText).type('16298561000119108')
         Utilities.waitForElementEnabled(CQLEditorPage.codeSystemSearchBtn, 30000)
         cy.get(CQLEditorPage.codeSystemSearchBtn).click()
         Utilities.waitForElementVisible(CQLEditorPage.codeSystemSearchResultsTbl, 30000)
         cy.get(CQLEditorPage.toolTip).trigger('mouseover')
         cy.get(CQLEditorPage.toolTipMsg).should('contain.text', 'This code is inactive in this code system version')
-        cy.get(CQLEditorPage.codeSystemSearchResultsTbl).should('contain.text', 'CodeDescriptionCode SystemSystem Version16298561000119108Administration of tetanus, diphtheria, and acellular pertussis vaccine (procedure)SNOMEDCThttp://snomed.info/sct/731000124108/version/20230901')
-
+        cy.get(CQLEditorPage.codeSystemSearchResultsTbl).should('contain.text', 'CodeDescriptionCode SystemSystem Version16298561000119108Administration of tetanus, diphtheria, and acellular pertussis vaccine (procedure)SNOMEDCThttp://snomed.info/sct/731000124108/version/20230301Select')
         //Clear the code search values
         cy.get(CQLEditorPage.clearCodeBtn).click()
 
@@ -94,6 +97,62 @@ describe.skip('QDM Code Search fields', () => {
         Utilities.waitForElementVisible(CQLEditorPage.codeSystemSearchResultsTbl, 30000)
         cy.get(CQLEditorPage.toolTip).trigger('mouseover')
         cy.get(CQLEditorPage.toolTipMsg).should('contain.text', 'Code status unavailable')
-        cy.get(CQLEditorPage.codeSystemSearchResultsTbl).should('contain.text', 'CodeDescriptionCode SystemSystem VersionAMBambulatoryActCode9.0.0')
+        cy.get(CQLEditorPage.codeSystemSearchResultsTbl).should('contain.text', 'CodeDescriptionCode SystemSystem VersionAMBambulatoryActCode9.0.0Select')
+    })
+
+    it('Apply code to the Measure validations', () => {
+
+        //Click on Edit Button
+        MeasuresPage.measureAction("edit")
+
+        //Save CQL
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('contain.text', 'CQL updated successfully! ' +
+            'Library Statement or Using Statement were incorrect. MADiE has overwritten them to ensure proper CQL.')
+
+        //Click on Codes tab
+        cy.get(CQLEditorPage.codesTab).click()
+
+        //Navigate to Code sub tab
+        cy.get(CQLEditorPage.codeSubTab).click()
+
+        //Search for the Code
+        cy.get(CQLEditorPage.codeSystemDropdown).type('SNOMEDCT')
+        cy.get(CQLEditorPage.codeSystemOptionListBox).contains('SNOMEDCT').click()
+        cy.get(CQLEditorPage.codeSystemVersionDropdown).click()
+        cy.get('[data-testid="http://snomed.info/sct/731000124108/version/20230301-option"]').click()
+        cy.get(CQLEditorPage.codeText).type('258219007')
+        Utilities.waitForElementEnabled(CQLEditorPage.codeSystemSearchBtn, 30000)
+        cy.get(CQLEditorPage.codeSystemSearchBtn).click()
+        Utilities.waitForElementVisible(CQLEditorPage.codeSystemSearchResultsTbl, 30000)
+        cy.get(CQLEditorPage.toolTip).trigger('mouseover')
+        cy.get(CQLEditorPage.toolTipMsg).should('contain.text', 'This code is active in this code system version')
+        cy.get(CQLEditorPage.codeSystemSearchResultsTbl).should('contain.text', 'CodeDescriptionCode SystemSystem Version258219007Stage 2 (qualifier value)SNOMEDCThttp://snomed.info/sct/731000124108/version/20230301Select')
+
+        //Apply code to the Measure
+        cy.get('[data-testid="select-action-0_apply"]').click()
+        cy.get('[class="btn-container"]').contains('Apply').click()
+        cy.get('[class="toast success"]').should('contain.text', 'Code 258219007 has been successfully added to the CQL.')
+
+        //Save and Discard changes button should be enabled after applying the code
+        cy.get(CQLEditorPage.saveCQLButton).should('be.enabled')
+        cy.get(EditMeasurePage.cqlEditorDiscardButton).should('be.enabled')
+
+        //Need to revisit once MAT-7180 is fixed
+        //Save CQL
+        cy.get(CQLEditorPage.saveCQLButton).click()
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('contain.text', 'CQL saved successfully')
+
+        //Assert toast message while trying to apply the same code again
+        cy.get('[data-testid="select-action-0_apply"]').click()
+        cy.get('[class="btn-container"]').contains('Apply').click()
+        cy.get('[class="toast success"]').should('contain.text', 'This code is already defined in the CQL.')
+
+        //Save and Discard changes button should be disabled
+        cy.get(CQLEditorPage.saveCQLButton).should('be.disabled')
+        cy.get(EditMeasurePage.cqlEditorDiscardButton).should('be.disabled')
     })
 })
