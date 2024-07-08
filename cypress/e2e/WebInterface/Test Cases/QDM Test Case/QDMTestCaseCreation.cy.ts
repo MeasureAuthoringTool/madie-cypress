3a4e7592a55d58ca89dfb368f17628401e4ba520
bwelch8105 <brandon.welch@semanticbits.com>
2024-07-08 09:34:59 -0500

    This PR updates what libraries are being used for some of our automated regression tests to use libraries that align with PROD, in CQL values.

--- cypress/e2e/WebInterface/Test Cases/QDM Test Case/QDMTestCaseCreation.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/QDM Test Case/QDMTestCaseCreation.cy.ts
@@ -26,8 +26,8 @@ let measureCQL = 'library BreastCancerScreening version \'12.0.000\'\n' +
     'include MATGlobalCommonFunctionsQDM version \'1.0.000\' called Global\n' +
     'include AdultOutpatientEncountersQDM version \'1.0.000\' called AdultOutpatientEncounters\n' +
     'include HospiceQDM version \'1.0.000\' called Hospice\n' +
-    'include PalliativeCareExclusionECQMQDM version \'1.0.000\' called PalliativeCare\n' +
-    'include AdvancedIllnessandFrailtyExclusionECQMQDM version \'1.0.000\' called AIFrailLTCF\n' +
+    'include PalliativeCareQDM version \'4.0.000\' called PalliativeCare\n' +
+    'include AdvancedIllnessandFrailtyQDM version \'9.0.000\' called AIFrailLTCF\n' +
     '\n' +
     'codesystem "AdministrativeGender": \'urn:oid:2.16.840.1.113883.5.1\' \n' +
     'codesystem "SNOMEDCT": \'urn:oid:2.16.840.1.113883.6.96\' \n' +
