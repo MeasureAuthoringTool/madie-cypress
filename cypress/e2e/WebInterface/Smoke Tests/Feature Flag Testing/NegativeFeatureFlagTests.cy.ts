d9566d31d02e6958eb3d0d532a5469b0ca5cb75a
praveenavas <prveena.vasireddy@icf.com>
2024-08-02 11:59:24 -0400

    MAT-7475 removed feature flag test cases that are no longer available

--- cypress/e2e/WebInterface/Smoke Tests/Feature Flag Testing/NegativeFeatureFlagTests.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/Feature Flag Testing/NegativeFeatureFlagTests.cy.ts
@@ -9,7 +9,6 @@ import { TestCasesPage } from "../../../../Shared/TestCasesPage"
 import { TestCaseJson } from "../../../../Shared/TestCaseJson"
 import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
 import { Header } from "../../../../Shared/Header"
-import { CQLLibraryPage } from "../../../../Shared/CQLLibraryPage";
 
 let randValue = (Math.floor((Math.random() * 1000) + 1))
 let qdmManifestTestCQL = MeasureCQL.qdmCQLManifestTest
@@ -22,15 +21,12 @@ let QDMCqlLibraryName0 = ''
 let measureQDMManifestName1 = ''
 let QDMCqlLibraryName1 = ''
 let measureName = 'TestMeasure' + Date.now()
-let filePath = 'cypress/fixtures/measureId'
 let CqlLibraryName = 'TestLibrary' + Date.now()
 let testCaseTitle = 'test case title'
 let testCaseDescription = 'DENOMFail' + Date.now()
-let measureCQL = MeasureCQL.QDMTestCaseCQLFullElementSection
 let testCaseSeries = 'SBTestSeries'
 let measureCQLAlt = MeasureCQL.ICFCleanTestQICore
 let cqlLibraryName = 'TestLibrary' + Date.now()
-let measureScoring = 'Cohort'
 let validTestCaseJsonLizzy = TestCaseJson.TestCaseJson_Valid
 let measureCQLPFTests = MeasureCQL.CQL_Populations
 
@@ -65,41 +61,6 @@ describe('Test Case Import button - BONNIE: verify that the BONNIE import button
         //confirm that the import button is disabled / not available
         Utilities.waitForElementToNotExist(TestCasesPage.bonnieImportTestCaseBtn, 35000)
 
-
-    })
-})
-
-// "qdmExport": true
-describe('QDM Measure Export: Export option is available', () => {
-
-    before('Create New Measure and Login', () => {
-
-        //Create New Measure
-        CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(measureName, CqlLibraryName, measureScoring, false, measureCQL)
-        //create Measure Group
-        MeasureGroupPage.CreateCohortMeasureGroupAPI(false, false, 'Initial Population', 'boolean')
-        OktaLogin.Login()
-
-    })
-
-    after('Logout and cleanup', () => {
-
-        OktaLogin.UILogout()
-        Utilities.deleteMeasure(measureName, CqlLibraryName)
-
-    })
-
-
-    it('QDM Measure Export: Export option for measure is available', () => {
-
-        cy.readFile(filePath).should('exist').then((fileContents) => {
-            cy.get('[data-testid="measure-action-' + fileContents + '"]').click()
-            cy.get('[data-testid="view-measure-' + fileContents + '"]').should('be.visible')
-            cy.get('[data-testid="export-measure-' + fileContents + '"]').should('exist')
-
-            cy.reload()
-            Utilities.waitForElementVisible(Header.userProfileSelect, 60000)
-        })
 
     })
 })
@@ -139,166 +100,6 @@ describe('QI Core: Elements tab is not present', () => {
 
         //Elements tab should not be visible
         Utilities.waitForElementToNotExist(TestCasesPage.elementsTab, 20000)
-    })
-})
-
-// "generateCMSID": true
-describe('QDM: Generate CMS ID', () => {
-
-    let randValue = (Math.floor((Math.random() * 1000) + 1))
-    let newMeasureName = 'TestMeasure' + Date.now() + randValue
-    let newCqlLibraryName = 'MeasureTypeTestLibrary' + Date.now() + randValue
-
-    before('Create Measure and Login', () => {
-
-        //Create New Measure
-        CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(newMeasureName, newCqlLibraryName, 'Cohort', true, measureCQL)
-        OktaLogin.Login()
-
-    })
-
-    after('Logout and cleanup', () => {
-
-        OktaLogin.UILogout()
-        Utilities.deleteMeasure(newMeasureName, newCqlLibraryName)
-
-    })
-
-    it('CMS Id generation for QDM Measure', () => {
-
-        MeasuresPage.measureAction('edit')
-        cy.get(EditMeasurePage.generateCmsIdButton).click()
-        //
-
-        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogCancel, 3500)
-        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogContinue, 3500)
-        cy.get(EditMeasurePage.cmsIDDialogCancel).click()
-        cy.get(EditMeasurePage.cmsIdInput).should('not.exist')
-        cy.get(EditMeasurePage.generateCmsIdButton).click()
-        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogCancel, 3500)
-        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogContinue, 3500)
-        cy.get(EditMeasurePage.cmsIDDialogContinue).click()
-
-        //
-        cy.get(EditMeasurePage.cmsIdInput).should('exist')
-
-    })
-})
-
-// "includeSDEValues": true
-describe('QDM: Configuration sub tab', () => {
-
-    let randValue = (Math.floor((Math.random() * 1000) + 1))
-    let newMeasureName = 'TestMeasure' + Date.now() + randValue
-    let newCqlLibraryName = 'MeasureTypeTestLibrary' + Date.now() + randValue
-
-    before('Create Measure and Login', () => {
-
-        //Create New Measure
-        CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(newMeasureName, newCqlLibraryName, 'Cohort', false, measureCQL)
-        OktaLogin.Login()
-        MeasuresPage.measureAction("edit")
-        cy.get(EditMeasurePage.cqlEditorTab).click()
-        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
-        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
-        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
-        cy.wait(3000)
-        OktaLogin.UILogout()
-        sessionStorage.clear()
-        cy.clearAllCookies()
-        cy.clearLocalStorage()
-        cy.setAccessTokenCookie().wait(3000)
-        //create Measure Group
-        MeasureGroupPage.CreateCohortMeasureGroupAPI(false, false, 'Initial Population')
-        sessionStorage.clear()
-        cy.clearAllCookies()
-        cy.clearLocalStorage()
-        cy.setAccessTokenCookie().wait(3000)
-        OktaLogin.Login()
-
-    })
-
-    after('Logout and cleanup', () => {
-
-        OktaLogin.UILogout()
-        Utilities.deleteMeasure(newMeasureName, newCqlLibraryName)
-
-    })
-
-    it('Configuration sub tab should be displayed on QDM Test Case list page', () => {
-
-        MeasuresPage.measureAction('edit')
-        cy.get(EditMeasurePage.testCasesTab).click()
-        Utilities.waitForElementVisible(TestCasesPage.newTestCaseButton, 60000)
-        cy.get(TestCasesPage.configurationSubTab).should('be.visible')
-
-    })
-})
-
-// "TestCaseExport" : true
-describe('QDM: Test Case Export button and it\s sub-action buttons', () => {
-
-    let randValue = (Math.floor((Math.random() * 1000) + 1))
-    let newMeasureName = 'TestMeasure' + Date.now() + randValue
-    let newCqlLibraryName = 'MeasureTypeTestLibrary' + Date.now() + randValue
-    let randValueQICore = (Math.floor((Math.random() * 100) + 1))
-    let qiCoreTestMeasureName = 'QICoreMeasure' + randValueQICore + Date.now()
-    let qiCoreTestCqlLibName = 'QICoreMeasure' + + randValueQICore + Date.now()
-
-    before('Create Measure and Login', () => {
-
-        //Create New Measure
-        CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(newMeasureName, newCqlLibraryName, 'Cohort', true, measureCQL)
-        OktaLogin.Login()
-
-    })
-
-    it('QDM Test case Export button is visible when the feature flag is set to true', () => {
-
-        // edit QDM measure and confirm that the Export button does not exist, at all, for QDM
-        MeasuresPage.measureAction('edit')
-        cy.get(EditMeasurePage.testCasesTab).click()
-        cy.get(TestCasesPage.testcaseQRDAExportBtn).should('exist')
-
-        // log out of MADiE UI
-        OktaLogin.UILogout()
-
-        // create QI Core Measure
-        CreateMeasurePage.CreateQICoreMeasureAPI(qiCoreTestMeasureName, qiCoreTestCqlLibName, measureCQLAlt)
-
-        // log back into MADiE UI
-        OktaLogin.Login()
-
-        //Click on Edit Measure and make insignificant edit to CQL and save
-        MeasuresPage.measureAction("edit")
-        cy.get(EditMeasurePage.cqlEditorTab).click()
-        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
-        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
-        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
-        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('contain.text', 'CQL updated successfully but the following issues were found')
-        cy.get(CQLLibraryPage.libraryWarning).should('contain.text', 'Library statement was incorrect. MADiE has overwritten it.')
-
-        // navigate to MADiE's homepage
-        cy.get(Header.measures).click()
-
-        // edit QI Core measure
-        MeasuresPage.measureAction('edit')
-
-        // navigate to the Test Case list page, for the QI Core Measure
-        cy.get(EditMeasurePage.testCasesTab).click()
-
-        // find and click on the export button that appears for the QI Core Measure's test case list page
-        cy.get(TestCasesPage.exportTestCasesBtn)
-            .find('[class="export-action"]')
-            .scrollIntoView()
-            .click({ force: true })
-
-        // confirm that only the action buttons that should appear are appearing on page
-        Utilities.waitForElementToNotExist(TestCasesPage.exportExcelBtn, 30000)
-        Utilities.waitForElementVisible(TestCasesPage.exportTransactionBundleBtn, 30000)
-        Utilities.waitForElementVisible(TestCasesPage.exportCollectionBundleBtn, 30000)
-
-
     })
 })
 
