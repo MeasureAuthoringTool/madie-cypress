5333fe88aa95b89c81adcfd0345af0b7666ff94f
praveenavas <82400011+praveenavas@users.noreply.github.com>
2024-09-06 11:55:39 -0400

    MAT-7465 Populate saved Libraries (#1353)
    * Updated QDM Measure Export test
    
    * Removed unnecessary waits
    
    * MAT-7465 Populate saved Libraries
    
    * Added .skip
    
    ---------
    
    Co-authored-by: praveenavas <prveena.vasireddy@icf.com>

--- cypress/e2e/WebInterface/Measure/QDM CQL Editor/QDMLibraryIncludes.cy.ts
+++ cypress/e2e/WebInterface/Measure/QDM CQL Editor/QDMLibraryIncludes.cy.ts
@@ -3,7 +3,6 @@ import {OktaLogin} from "../../../../Shared/OktaLogin"
 import {MeasuresPage} from "../../../../Shared/MeasuresPage"
 import {EditMeasurePage} from "../../../../Shared/EditMeasurePage"
 import {CQLEditorPage} from "../../../../Shared/CQLEditorPage"
-import {CQLLibraryPage} from "../../../../Shared/CQLLibraryPage"
 import {Utilities} from "../../../../Shared/Utilities"
 
 let measureName = 'QDMTestMeasure' + Date.now()
@@ -11,6 +10,7 @@ let CqlLibraryName = 'QDMLibrary' + Date.now()
 let measureCQL = 'library TestLibrary1685544523170534 version \'0.0.000\'\n' +
     'using QDM version \'5.6\'\n' +
     '\n' +
+    'include MATGlobalCommonFunctionsQDM version \'8.0.000\' called Common\n' +
     'valueset "Ethnicity": \'urn:oid:2.16.840.1.114222.4.11.837\'\n' +
     'valueset "ONC Administrative Sex": \'urn:oid:2.16.840.1.113762.1.4.1\'\n' +
     'valueset "Payer": \'urn:oid:2.16.840.1.114222.4.11.3591\'\n' +
@@ -47,12 +47,7 @@ describe.skip('QDM Library Includes fields', () => {
 
         //Save CQL
         cy.get(EditMeasurePage.cqlEditorTab).click()
-        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
-        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
-        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
-        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('contain.text', 'CQL updated successfully but the following issues were found')
-        cy.get(CQLLibraryPage.libraryWarning).should('contain.text', 'Library statement was incorrect. MADiE has overwritten it.')
-
+        cy.get(CQLEditorPage.expandCQLBuilder).click()
     })
 
     afterEach('Clean up and Logout', () => {
@@ -70,11 +65,25 @@ describe.skip('QDM Library Includes fields', () => {
         //Search for FHIR Library
         cy.get(CQLEditorPage.librarySearchTextBox).type('fhir')
         cy.get(CQLEditorPage.librarySearchBtn).click()
-        cy.get('[class="Results___StyledTd-sc-wvb0lh-0 jvRQdT"]').should('contain.text', 'No Results were found')
+        cy.get('.Results___StyledTd-sc-18pioce-0').should('contain.text', 'No Results were found')
 
         //Search for QDM Libraries
         cy.get(CQLEditorPage.librarySearchTextBox).clear().type('sdoh')
         cy.get(CQLEditorPage.librarySearchBtn).click().wait(1000)
         cy.get(CQLEditorPage.librarySearchTable).should('contain','nameversionownerActionSDOH3.0.000ltj7708SDOH2.0.000ltj7708SDOH1.0.000ltj7708')
     })
+
+    it('Verify Included Libraries under Saved Libraries tab', () => {
+
+        //Click on Includes tab
+        cy.get(CQLEditorPage.includesTab).click()
+
+        //Navigate to Saved Libraries tab
+        cy.get(CQLEditorPage.savedLibrariesTab).click()
+        cy.get(CQLEditorPage.savedLibrariesTable).should('contain.text', 'Common')//Alias
+        cy.get(CQLEditorPage.savedLibrariesTable).should('contain.text', 'MATGlobalCommonFunctionsQDM')//Name
+        cy.get(CQLEditorPage.savedLibrariesTable).should('contain.text', '8.0.000')//Version
+        cy.get(CQLEditorPage.savedLibrariesTable).should('contain.text', 'julietrubini')//Owner
+    })
+
 })
