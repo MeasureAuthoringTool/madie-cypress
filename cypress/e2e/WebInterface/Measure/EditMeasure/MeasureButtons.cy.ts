d1fa6d752c509e9a62b1f62bf004616e86e1fecd
Colin Sulllivan <n119493@icf.com>
2024-11-11 16:17:07 -0600

    page and test

--- cypress/e2e/WebInterface/Measure/EditMeasure/MeasureButtons.cy.ts
+++ cypress/e2e/WebInterface/Measure/EditMeasure/MeasureButtons.cy.ts
@@ -1,12 +1,15 @@
 import { OktaLogin } from "../../../../Shared/OktaLogin"
 import { CreateMeasurePage } from "../../../../Shared/CreateMeasurePage"
 import { MeasuresPage } from "../../../../Shared/MeasuresPage"
-import { EditMeasurePage } from "../../../../Shared/EditMeasurePage"
+import { EditMeasurePage, EditMeasureActions } from "../../../../Shared/EditMeasurePage"
 import { Utilities } from "../../../../Shared/Utilities"
 import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
 import { MeasureCQL } from "../../../../Shared/MeasureCQL"
 import { MeasureGroupPage } from "../../../../Shared/MeasureGroupPage"
 import { Header } from "../../../../Shared/Header"
+const path = require('path')
+const downloadsFolder = Cypress.config('downloadsFolder')
+const { deleteDownloadsFolderBeforeEach } = require('cypress-delete-downloads-folder')
 
 let randValue = (Math.floor((Math.random() * 1000) + 1))
 let measureCQLPFTests = MeasureCQL.CQL_Populations
@@ -217,5 +220,67 @@ describe('Version and Draft Qi Core Measure on the Edit Measure page', () => {
         cy.get(EditMeasurePage.editPageVersionDraftMsg).should('contain.text', 'New draft created successfully.')
 
         cy.log('Draft Created Successfully')
+    })
+})
+
+describe('Export measure on the Edit Measure page', () => {
+
+    deleteDownloadsFolderBeforeEach()
+
+    before(() => {
+
+        cy.clearAllCookies()
+        cy.clearLocalStorage()
+        cy.setAccessTokenCookie()
+
+        const date = Date.now()
+        measureQDM = 'QDMExportMeasure' + date
+        CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(measureQDM, measureQDM, 'Proportion', false, qdmManifestTestCQL, null, false,
+            '2025-01-01', '2025-12-31')
+        MeasureGroupPage.CreateProportionMeasureGroupAPI(null, false, 'Initial Population', '', 'Denominator Exceptions',
+            'Numerator', '', 'Denominator')
+
+        measureQICore = 'QICoreExportMeasure' + date
+        CreateMeasurePage.CreateQICoreMeasureAPI(measureQICore, measureQICore, measureCQLPFTests, 1)
+        MeasureGroupPage.CreateProportionMeasureGroupAPI(1, false, 'Initial Population', '', '',
+            'Initial Population', '', 'Initial Population', 'boolean')
+    })
+
+    after(() => {
+
+        Utilities.deleteMeasure(measureQDM, measureQDM)
+        Utilities.deleteMeasure(measureQICore, measureQICore, false, false, 1)
+    })
+
+    it('Export QDM 5.6 measure', () => {
+        const exportName = 'eCQMTitle4QDM-v0.0.000-QDM.zip'
+        const fullPathToExport = path.join(downloadsFolder, exportName)
+
+        OktaLogin.Login()
+
+        MeasuresPage.actionCenter("edit")
+
+        Utilities.waitForElementVisible(EditMeasurePage.cqlLibraryNameTextBox, 15500)
+    
+        EditMeasurePage.actionCenter(EditMeasureActions.export)
+    
+        cy.readFile(fullPathToExport, { timeout: 500000 }).should('exist')
+        cy.log('Successfully verified zip file export')
+    })
+
+    it('Export QiCore 4.1.1 measure', () => {
+        const exportName = 'eCQMTitle4QICore-v0.0.000-FHIR4.zip'
+        const fullPathToExport = path.join(downloadsFolder, exportName)
+
+        OktaLogin.Login()
+
+        MeasuresPage.actionCenter("edit", 1)
+
+        Utilities.waitForElementVisible(EditMeasurePage.cqlLibraryNameTextBox, 15500)
+    
+        EditMeasurePage.actionCenter(EditMeasureActions.export)
+    
+        cy.readFile(fullPathToExport, { timeout: 500000 }).should('exist')
+        cy.log('Successfully verified zip file export')
     })
 })
