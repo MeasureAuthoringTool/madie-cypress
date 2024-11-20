eb18c676ba80fabc9c168e9c01edd0c8dde5dc4d
praveenavas <prveena.vasireddy@icf.com>
2024-11-20 15:53:18 -0500

    Fix for TestCaseJSON_TerminologyTests.cy.ts

--- cypress/e2e/WebInterface/Test Cases/QI-CORE Test Case/TestCaseJSON_TerminologyTests.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/QI-CORE Test Case/TestCaseJSON_TerminologyTests.cy.ts
@@ -17,7 +17,6 @@ let testCaseDescription = 'DENOMFail' + Date.now()
 let missingResourceIDTCJson = TestCaseJson.TestCaseJson_missingResourceIDs
 let missingResourceIDTCJsonButHasFullUrlExt = TestCaseJson.TestCaseJson_missingResourceIDsHasfullUrlExt
 let emptyResourceIDTCJson = TestCaseJson.TestCaseJson_emptyResourceIDs
-let dupResourceIDTCJson = TestCaseJson.TestCaseJson_resourceIDsDup
 let missingMetaProfile = TestCaseJson.TestCaseJson_missingMetaProfile
 let validTestCaseJson = TestCaseJson.TestCaseJson_Valid
 let cardInvalidTCJson = TestCaseJson.tcCardErrorJson
@@ -109,7 +108,210 @@ let measureCQLCardTests = 'library TestLibrary1678378360032 version \'0.0.000\'\
     '\n' +
     'define "Denom":\n' +
     'true'
-
+let dupResourceIDTCJson = '{\n' +
+    '  "resourceType": "Bundle",\n' +
+    '  "id": "ip-pass-InpatientEncounter",\n' +
+    '  "meta": {\n' +
+    '    "versionId": "3",\n' +
+    '    "lastUpdated": "2022-09-14T12:38:39.889+00:00"\n' +
+    '  },\n' +
+    '  "type": "collection",\n' +
+    '  "entry": [\n' +
+    '    {\n' +
+    '      "fullUrl": "https://madie.cms.gov/Patient/27b358b4-d520-422b-8ed7-a5b14f3d2a46",\n' +
+    '      "resource": {\n' +
+    '        "id": "27b358b4-d520-422b-8ed7-a5b14f3d2a46",\n' +
+    '        "resourceType": "Patient",\n' +
+    '        "meta": {\n' +
+    '          "profile": [\n' +
+    '            "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-patient"\n' +
+    '          ]\n' +
+    '        },\n' +
+    '        "identifier": [\n' +
+    '          {\n' +
+    '            "type": {\n' +
+    '              "coding": [\n' +
+    '                {\n' +
+    '                  "system": "http://terminology.hl7.org/CodeSystem/v2-0203",\n' +
+    '                  "code": "MR"\n' +
+    '                }\n' +
+    '              ]\n' +
+    '            },\n' +
+    '            "system": "http://myGoodHealthcare.com/MRN",\n' +
+    '            "value": "8065dc8d26797064d8766be71f2bf020"\n' +
+    '          }\n' +
+    '        ],\n' +
+    '        "active": true,\n' +
+    '        "name": [\n' +
+    '          {\n' +
+    '            "use": "usual",\n' +
+    '            "family": "IPPass",\n' +
+    '            "given": [\n' +
+    '              "Inpatient Encounter"\n' +
+    '            ]\n' +
+    '          }\n' +
+    '        ],\n' +
+    '        "gender": "male",\n' +
+    '        "birthDate": "1954-02-10"\n' +
+    '      }\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "fullUrl": "https://madie.cms.gov/Encounter/4989ju789fn93bvy562loe87c",\n' +
+    '      "resource": {\n' +
+    '        "id": "4989ju789fn93bvy562loe87c",\n' +
+    '        "meta": {\n' +
+    '          "profile": [\n' +
+    '            "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-encounter"\n' +
+    '          ]\n' +
+    '        },\n' +
+    '        "resourceType": "Encounter",\n' +
+    '        "status": "finished",\n' +
+    '        "class": {\n' +
+    '          "system": "http://terminology.hl7.org/CodeSystem/v3-ActCode",\n' +
+    '          "code": "IMP",\n' +
+    '          "display": "inpatient encounter"\n' +
+    '        },\n' +
+    '        "type": [\n' +
+    '          {\n' +
+    '            "coding": [\n' +
+    '              {\n' +
+    '                "system": "http://snomed.info/sct",\n' +
+    '                "code": "183452005",\n' +
+    '                "display": "Emergency hospital admission (procedure)"\n' +
+    '              }\n' +
+    '            ]\n' +
+    '          }\n' +
+    '        ],\n' +
+    '        "subject": {\n' +
+    '          "reference": "Patient/27b358b4-d520-422b-8ed7-a5b14f3d2a46"\n' +
+    '        },\n' +
+    '        "priority": {\n' +
+    '          "coding": [\n' +
+    '            {\n' +
+    '              "system": "http://snomed.info/sct",\n' +
+    '              "code": "103391001",\n' +
+    '              "display": "Urgency"\n' +
+    '            }\n' +
+    '          ]\n' +
+    '        },\n' +
+    '        "period": {\n' +
+    '          "start": "2012-07-15T08:00:00+00:00",\n' +
+    '          "end": "2012-07-16T09:00:00+00:00"\n' +
+    '        },\n' +
+    '        "length": {\n' +
+    '          "value": 1,\n' +
+    '          "unit": "days"\n' +
+    '        },\n' +
+    '        "location": [\n' +
+    '          {\n' +
+    '            "location": {\n' +
+    '              "reference": "Location/4989ju789fn93bvy562loe87c",\n' +
+    '              "display": "Holy Family Hospital Inpatient"\n' +
+    '            },\n' +
+    '            "period": {\n' +
+    '              "start": "2012-07-15T08:00:00+00:00",\n' +
+    '              "end": "2012-07-16T09:00:00+00:00"\n' +
+    '            }\n' +
+    '          }\n' +
+    '        ]\n' +
+    '      }\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "fullUrl": "https://madie.cms.gov/Encounter/9dju7njdn764mdjy6dm92nje",\n' +
+    '      "resource": {\n' +
+    '        "id": "9dju7njdn764mdjy6dm92nje",\n' +
+    '        "resourceType": "Encounter",\n' +
+    '        "meta": {\n' +
+    '          "profile": [\n' +
+    '            "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-encounter"\n' +
+    '          ]\n' +
+    '        },\n' +
+    '        "status": "finished",\n' +
+    '        "class": {\n' +
+    '          "system": "http://terminology.hl7.org/CodeSystem/v3-ActCode",\n' +
+    '          "code": "EMRGONLY",\n' +
+    '          "display": "Emergency only"\n' +
+    '        },\n' +
+    '        "type": [\n' +
+    '          {\n' +
+    '            "coding": [\n' +
+    '              {\n' +
+    '                "system": "http://snomed.info/sct",\n' +
+    '                "code": "4525004",\n' +
+    '                "display": "Emergency department patient visit (procedure)"\n' +
+    '              }\n' +
+    '            ]\n' +
+    '          }\n' +
+    '        ],\n' +
+    '        "subject": {\n' +
+    '          "reference": "Patient/27b358b4-d520-422b-8ed7-a5b14f3d2a46"\n' +
+    '        },\n' +
+    '        "period": {\n' +
+    '          "start": "2012-07-14T23:00:00+00:00",\n' +
+    '          "end": "2012-07-15T07:30:00+00:00"\n' +
+    '        },\n' +
+    '        "length": {\n' +
+    '          "value": 1,\n' +
+    '          "unit": "days"\n' +
+    '        },\n' +
+    '        "location": [\n' +
+    '          {\n' +
+    '            "location": {\n' +
+    '              "reference": "Location/489juh6757h87j03jhy73mv7",\n' +
+    '              "display": "Holy Family Hospital Inpatient"\n' +
+    '            },\n' +
+    '            "period": {\n' +
+    '              "start": "2012-07-14T23:00:00+00:00",\n' +
+    '              "end": "2012-07-15T07:30:00+00:00"\n' +
+    '            }\n' +
+    '          }\n' +
+    '        ]\n' +
+    '      }\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "fullUrl": "https://madie.cms.gov/Location/489juh6757h87j03jhy73mv7",\n' +
+    '      "resource": {\n' +
+    '        "id": "489juh6757h87j03jhy73mv7",\n' +
+    '        "resourceType": "Location",\n' +
+    '        "meta": {\n' +
+    '          "profile": [\n' +
+    '            "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-location"\n' +
+    '          ]\n' +
+    '        },\n' +
+    '        "identifier": [\n' +
+    '          {\n' +
+    '            "use": "official",\n' +
+    '            "system": "http://holycrosshospital.com/location",\n' +
+    '            "value": "489juh6757h87j03jhy73mv7"\n' +
+    '          }\n' +
+    '        ],\n' +
+    '        "status": "active",\n' +
+    '        "name": "South Wing, second floor"\n' +
+    '      }\n' +
+    '    },\n' +
+    '    {\n' +
+    '      "fullUrl": "https://madie.cms.gov/Location/4989ju789fn93bvy562loe87c",\n' +
+    '      "resource": {\n' +
+    '        "id": "4989ju789fn93bvy562loe87c",\n' +
+    '        "resourceType": "Location",\n' +
+    '        "meta": {\n' +
+    '          "profile": [\n' +
+    '            "http://hl7.org/fhir/us/qicore/StructureDefinition/qicore-location"\n' +
+    '          ]\n' +
+    '        },\n' +
+    '        "identifier": [\n' +
+    '          {\n' +
+    '            "use": "official",\n' +
+    '            "system": "http://holycrosshospital.com/location",\n' +
+    '            "value": "4989ju789fn93bvy562loe87c"\n' +
+    '          }\n' +
+    '        ],\n' +
+    '        "status": "active",\n' +
+    '        "name": "North Wing, second floor"\n' +
+    '      }\n' +
+    '    }\n' +
+    '  ]\n' +
+    '}'
 describe('Warning modal on Test Case JSON Editor', () => {
 
     beforeEach('Create measure and login', () => {
@@ -588,10 +790,9 @@ describe('JSON Resource ID tests', () => {
         cy.get(TestCasesPage.errorToastMsg).should('be.visible')
         cy.get(TestCasesPage.errorToastMsg).should('have.text', 'Test case updated successfully with errors in JSON')
 
-        cy.get(TestCasesPage.testCaseJsonValidationErrorBtn).click()
         cy.get(TestCasesPage.testCaseJsonValidationDisplayList).should('exist')
         cy.get(TestCasesPage.testCaseJsonValidationDisplayList).should('be.visible')
-        cy.get(TestCasesPage.testCaseJsonValidationDisplayList).should('contain.text', 'All resources in bundle must have unique ID regardless of type. Multiple resources detected with ID [1]')
+        cy.get(TestCasesPage.testCaseJsonValidationDisplayList).should('contain.text', 'Error: All resources in bundle must have unique ID regardless of type. Multiple resources detected with ID [4989ju789fn93bvy562loe87c]')
     })
 
     it('Verify warning message for missing Meta.Profile Values on Resources in Test case Json', () => {
