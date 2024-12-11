396e2146bca400ba402599526eee8dffc3aaf747
praveenavas <prveena.vasireddy@icf.com>
2024-12-11 10:01:53 -0500

    MAT-7938 Delete CMS ID

---
+++ cypress/e2e/Services/Measure Service/DeleteCMSID.cy.ts
@@ -0,0 +1,78 @@
+import {CreateMeasurePage} from "../../../Shared/CreateMeasurePage"
+import {OktaLogin} from "../../../Shared/OktaLogin"
+import {Utilities} from "../../../Shared/Utilities"
+import {MeasuresPage} from "../../../Shared/MeasuresPage"
+import {EditMeasurePage} from "../../../Shared/EditMeasurePage"
+import {Environment} from "../../../Shared/Environment"
+
+let measureName = 'TestMeasure' + Date.now()
+let CqlLibraryName = 'TestLibrary' + Date.now()
+let measureSharingAPIKey = Environment.credentials().measureSharing_API_Key
+
+describe('Delete CMS ID for QI-Core Measure', () => {
+
+    before('Login', () => {
+
+        let cmsId: string
+
+        //Create New Measure
+        CreateMeasurePage.CreateQDMMeasureAPI(measureName, CqlLibraryName)
+
+        OktaLogin.Login()
+
+        //Generate CMS ID
+        MeasuresPage.actionCenter('edit')
+        cy.get(EditMeasurePage.generateCmsIdButton).should('exist')
+        cy.get(EditMeasurePage.generateCmsIdButton).should('be.enabled')
+        cy.get(EditMeasurePage.cmsIdInput).should('not.exist')
+        cy.get(EditMeasurePage.generateCmsIdButton).click()
+        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogCancel, 3500)
+        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogContinue, 3500)
+        cy.get(EditMeasurePage.cmsIDDialogCancel).click()
+        cy.get(EditMeasurePage.cmsIdInput).should('not.exist')
+        cy.get(EditMeasurePage.generateCmsIdButton).click()
+        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogCancel, 3500)
+        Utilities.waitForElementVisible(EditMeasurePage.cmsIDDialogContinue, 3500)
+        cy.get(EditMeasurePage.cmsIDDialogContinue).click()
+
+        cy.get(EditMeasurePage.cmsIdInput).should('exist')
+        cy.get(EditMeasurePage.cmsIdInput).invoke('val').then(val => {
+            cmsId = val.toString().valueOf()
+            cy.writeFile('cypress/fixtures/cmsId', cmsId)
+        })
+        cy.log('CMS ID Generated successfully')
+
+    })
+
+    after('Log out and Clean up', () => {
+
+        OktaLogin.Logout()
+        Utilities.deleteMeasure(measureName, CqlLibraryName)
+
+    })
+    it('Verify that the CMS ID deleted successfully for QDM Measure', () => {
+
+        cy.clearCookies()
+        cy.clearLocalStorage()
+        cy.setAccessTokenCookie()
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((measureId) => {
+                cy.readFile('cypress/fixtures/cmsId').should('exist').then((cmsId) => {
+                    cy.readFile('cypress/fixtures/measureSetId').should('exist').then((measureSetId) => {
+                        cy.request({
+                            url: '/api/measures/' + measureId + '/delete-cms-id?cmsId=' + cmsId,
+                            method: 'DELETE',
+                            headers: {
+                                authorization: 'Bearer ' + accessToken.value,
+                                'api-key': measureSharingAPIKey
+                            }
+                        }).then((response) => {
+                            expect(response.status).to.eql(200)
+                            expect(response.body).to.eql('CMS id of ' + cmsId + ' was deleted successfully from measure set with measure set id of ' + measureSetId)
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
