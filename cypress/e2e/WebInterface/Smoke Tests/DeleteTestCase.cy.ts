68a542d5ae687cea6ccc6d74b0ee2896dbda36e5
Colin Sulllivan <n119493@icf.com>
2025-01-22 10:44:01 -0600

    feat: updates tests for stories

--- cypress/e2e/WebInterface/Smoke Tests/DeleteTestCase.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/DeleteTestCase.cy.ts
@@ -3,85 +3,86 @@ import { CreateMeasurePage } from "../../../Shared/CreateMeasurePage"
 import { OktaLogin } from "../../../Shared/OktaLogin"
 import { Utilities } from "../../../Shared/Utilities"
 import { MeasuresPage } from "../../../Shared/MeasuresPage"
-import { TestCasesPage } from "../../../Shared/TestCasesPage"
+import { TestCase, TestCasesPage } from "../../../Shared/TestCasesPage"
 import { EditMeasurePage } from "../../../Shared/EditMeasurePage"
-import { CQLEditorPage } from "../../../Shared/CQLEditorPage"
 import { MeasureGroupPage } from "../../../Shared/MeasureGroupPage"
 import { MeasureCQL } from "../../../Shared/MeasureCQL"
 
-let measureName = 'TestMeasure' + Date.now()
-let CqlLibraryName = 'TestLibrary' + Date.now()
-let measureCQL = MeasureCQL.ICFCleanTest_CQL
-let testCaseTitle = 'Title for Auto Test'
-let testCaseDescription = 'DENOMFail' + Date.now()
-let testCaseSeries = 'SBTestSeries'
-let testCaseJson = TestCaseJson.TestCaseJson_Valid_w_All_Encounter
-let newMeasureName: string
-let newCqlLibraryName: string
+const now = Date.now()
+let measureName = 'DeleteTC' + now
+let CqlLibraryName = 'DeleteTCLib' + now
+let measureCQL = MeasureCQL.ICFCleanTest_CQL.replace('SimpleFhirLibrary', CqlLibraryName)
+const testCase1: TestCase = {
+    title: 'Title for Auto Test',
+    description: 'DENOMFail test case',
+    group: 'SBTestSeries',
+    json: TestCaseJson.TestCaseJson_Valid_w_All_Encounter
+}
+const testCase2: TestCase = {
+    title: 'Another Title',
+    description: 'Successful test case',
+    group: 'SBTestSeries'
+}
 
 describe('Delete Test Case', () => {
 
     beforeEach('Create measure and login', () => {
 
-        let randValue = (Math.floor((Math.random() * 1000) + 1))
-        newMeasureName = measureName + randValue
-        newCqlLibraryName = CqlLibraryName + randValue
-
-        //Create New Measure
         CreateMeasurePage.CreateQICoreMeasureAPI(measureName, CqlLibraryName, measureCQL, null, false,
             '2012-01-02', '2013-01-01')
-        OktaLogin.Login()
-        MeasuresPage.actionCenter("edit")
-        cy.get(EditMeasurePage.cqlEditorTab).click()
-        cy.get(EditMeasurePage.cqlEditorTextBox).type('{enter}')
-        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
-        //wait for alert / successful save message to appear
-        Utilities.waitForElementVisible(CQLEditorPage.successfulCQLSaveNoErrors, 20700)
-        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
-        OktaLogin.Logout()
         MeasureGroupPage.CreateProportionMeasureGroupAPI(null, null, null, null, null, null,
             null, null, 'Procedure')
         OktaLogin.Login()
-
     })
 
     afterEach('Logout and Clean up Measures', () => {
 
         OktaLogin.UILogout()
-
-        let randValue = (Math.floor((Math.random() * 1000) + 1))
-        let newCqlLibraryName = CqlLibraryName + randValue
-
-        cy.clearAllCookies()
-        cy.clearLocalStorage()
-        cy.setAccessTokenCookie()
-        cy.clearAllSessionStorage({ log: true })
-
-        Utilities.deleteMeasure(newMeasureName, newCqlLibraryName)
-
+        Utilities.deleteMeasure(measureName, CqlLibraryName)
     })
 
-    it('Delete Test Case - Success scenario', () => {
+    it('Delete single Test Case - Success scenario', () => {
 
         MeasuresPage.actionCenter("edit")
 
-        TestCasesPage.createTestCase(testCaseTitle, testCaseDescription, testCaseSeries, testCaseJson)
+        cy.get(TestCasesPage.deleteAllTestCasesBtn).should('not.exist')
+        cy.get(TestCasesPage.exportTestCasesBtn).should('not.exist')
 
-        cy.get(TestCasesPage.selectTestCaseDropdownBtn).click()
-        TestCasesPage.clickDeleteTestCaseButton()
+        TestCasesPage.createTestCase(testCase1.title, testCase1.description, testCase1.group)
 
-        cy.get(TestCasesPage.deleteTestCaseConfirmationText).should('contain.text', 'Are you sure you want to delete ' + testCaseTitle + '?')
+        TestCasesPage.checkTestCase(1)
+        cy.get(TestCasesPage.actionCenterDelete).click()
+
+        //cy.get(TestCasesPage.deleteTestCaseConfirmationText).should('contain.text', 'Are you sure you want to delete ' + testCase1.title + '?')
+        cy.get(TestCasesPage.deleteTestCaseConfirmationText).should('contain.text', 'Are you sure you want to delete ?')
         cy.get(TestCasesPage.deleteTestCaseContinueBtn).click()
 
-        cy.get(TestCasesPage.testCaseListTable).should('not.contain', testCaseTitle)
+        cy.get(TestCasesPage.testCaseListTable).should('not.contain', testCase1.title)
+    })
 
+    it.only('Delete multiple Test Cases - Success scenario', () => {
+
+        MeasuresPage.actionCenter("edit")
+
+        TestCasesPage.createTestCase(testCase1.title, testCase1.description, testCase1.group)
+        TestCasesPage.createTestCase(testCase2.title, testCase2.description, testCase2.group)
+
+        TestCasesPage.checkTestCase(1)
+        TestCasesPage.checkTestCase(2)
+        cy.get(TestCasesPage.actionCenterDelete).click()
+
+        cy.get(TestCasesPage.deleteTestCaseConfirmationText).should('contain.text', 'Are you sure you want to delete ?')
+        cy.get(TestCasesPage.deleteTestCaseContinueBtn).click()
+
+        cy.get(TestCasesPage.testCaseListTable).should('not.contain', testCase1.title)
+        cy.get(TestCasesPage.testCaseListTable).should('not.contain', testCase2.title)
     })
 
     it('Verify Non owner of the Measure unable to delete Test Case', () => {
 
         MeasuresPage.actionCenter("edit")
 
-        TestCasesPage.createTestCase(testCaseTitle, testCaseDescription, testCaseSeries, testCaseJson)
+        TestCasesPage.createTestCase(testCase1.title, testCase1.description, testCase1.group, testCase1.json)
 
         OktaLogin.Logout()
 
@@ -89,16 +90,18 @@ describe('Delete Test Case', () => {
         OktaLogin.AltLogin()
         Utilities.waitForElementVisible(MeasuresPage.allMeasuresTab, 20700)
         cy.get(MeasuresPage.allMeasuresTab).click({ force: true })
-        cy.reload()
 
         MeasuresPage.actionCenter("edit")
 
         cy.get(EditMeasurePage.testCasesTab).click()
 
-        cy.get(TestCasesPage.selectTestCaseDropdownBtn).click()
-        cy.readFile('cypress/fixtures/testCaseId').should('exist').then((fileContents) => {
-            cy.get('[data-testid=delete-test-case-btn-' + fileContents + ']').should('not.exist')
-            cy.get('[data-testid=view-edit-test-case-' + fileContents + ']').click()
-        })
+        TestCasesPage.checkTestCase(1)
+        cy.get(TestCasesPage.actionCenterDelete).should('not.exist')
+        cy.get(TestCasesPage.importTestCaseBtn).should('not.exist')
+        cy.get(TestCasesPage.newTestCaseButton).should('not.exist')
+
+        cy.get(TestCasesPage.actionCenterExport).should('be.enabled')
+        cy.get(TestCasesPage.executeTestCaseButton).should('be.enabled')
+        cy.contains('View').should('be.visible')
     })
 })
