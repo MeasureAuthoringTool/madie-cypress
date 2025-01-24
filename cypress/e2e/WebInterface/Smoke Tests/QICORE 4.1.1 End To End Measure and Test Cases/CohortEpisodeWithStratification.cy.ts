3965a6dfc916f96a5f763e2de532288123b208d5
Colin Sulllivan <n119493@icf.com>
2025-01-24 15:04:23 -0600

    feat: replace hardcoded cql per test with shared

--- cypress/e2e/WebInterface/Smoke Tests/QICORE 4.1.1 End To End Measure and Test Cases/CohortEpisodeWithStratification.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/QICORE 4.1.1 End To End Measure and Test Cases/CohortEpisodeWithStratification.cy.ts
@@ -7,6 +7,7 @@ import { EditMeasurePage } from "../../../../Shared/EditMeasurePage"
 import { TestCasesPage } from "../../../../Shared/TestCasesPage"
 import { MeasuresPage } from "../../../../Shared/MeasuresPage"
 import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
+import { QiCore4Cql } from "../../../../Shared/FHIRMeasuresCQL"
 
 let measureName = 'CohortEpisodeWithStrat' + Date.now()
 let CqlLibraryName = 'CohortEpisodeWithStrat' + Date.now()
@@ -14,45 +15,7 @@ let testCaseTitle = 'PASS'
 let testCaseDescription = 'PASS' + Date.now()
 let testCaseSeries = 'SBTestSeries'
 let testCaseJson = TestCaseJson.TestCaseJson_CohortEpisodeWithStrat_PASS
-
-let measureCQL = 'library CohortEpisodeWithStrat1668108456699 version \'0.0.000\'\n' +
-    'using QICore version \'4.1.1\'\n' +
-    'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
-    'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\'\n' +
-    'valueset "Annual Wellness Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1240\'\n' +
-    'valueset "Preventive Care Services - Established Office Visit, 18 and Up": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1025\'\n' +
-    'valueset "Preventive Care Services-Initial Office Visit, 18 and Up": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1023\'\n' +
-    'valueset "Home Healthcare Services": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1016\'\n' +
-    'valueset "End Stage Renal Disease": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.353\'\n' +
-    '\n' +
-    'parameter "Measurement Period" Interval<DateTime>\n' +
-    'default Interval[@2019-01-01T00:00:00.0, @2020-01-01T00:00:00.0)\n' +
-    '\n' +
-    'context Patient\n' +
-    '\n' +
-    '\n' +
-    'define "Initial Population":\n' +
-    ' "Qualifying Encounters"\n' +
-    ' \n' +
-    'define "Qualifying Encounters":\n' +
-    '(\n' +
-    '[Encounter: "Office Visit"]\n' +
-    'union [Encounter: "Annual Wellness Visit"]\n' +
-    'union [Encounter: "Preventive Care Services - Established Office Visit, 18 and Up"]\n' +
-    'union [Encounter: "Preventive Care Services-Initial Office Visit, 18 and Up"]\n' +
-    'union [Encounter: "Home Healthcare Services"]\n' +
-    ') ValidEncounter\n' +
-    'where ValidEncounter.period during "Measurement Period"\n' +
-    'and ValidEncounter.isFinishedEncounter()\n' +
-    '\n' +
-    '\n' +
-    '\n' +
-    'define fluent function "isFinishedEncounter"(Enc Encounter):\n' +
-    '(Enc E where E.status = \'finished\') is not null\n' +
-    '\n' +
-    'define "Stratificaction 1":\n' +
-    ' "Qualifying Encounters" Enc\n' +
-    ' where Enc.type in "Annual Wellness Visit"'
+let measureCQL = QiCore4Cql.EpisodeWithStrat
 
 //MAT-7727
 describe('Measure Creation and Testing: Cohort Episode w/ Stratification', () => {
