ce1be7fd730dd837376f4804874fe914957bcb50
praveenavas <82400011+praveenavas@users.noreply.github.com>
2025-01-27 16:42:38 -0500

    Praveena-QA-MADiE11 (#1691)
    * Fix for QDMRunExecuteTC.cy.ts
    
    * Fix for QDMTestCaseImportViaFile.cy.ts
    
    * Fix for QDMShiftTestCaseDatesValidations.cy.ts and Qi-CoreShiftTestCaseDatesValidations.cy.ts
    
    ---------
    
    Co-authored-by: praveenavas <prveena.vasireddy@icf.com>

--- cypress/e2e/WebInterface/Test Cases/QDM Test Case/ShiftTestCaseDates/Qi-CoreShiftTestCaseDatesValidations.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/QDM Test Case/ShiftTestCaseDates/Qi-CoreShiftTestCaseDatesValidations.cy.ts
@@ -133,7 +133,8 @@ describe('Shift Test Case Dates tests for Qi-Core Measure', () => {
         })
     })
 
-    it('MADiE Shift Test Case Dates -> Shift single / specific test case\'s dates for Qi-Core Measure', () => {
+    //Skipping until MAT-8194 is finished
+    it.skip('MADiE Shift Test Case Dates -> Shift single / specific test case\'s dates for Qi-Core Measure', () => {
 
         //Click on Edit Measure
         MeasuresPage.actionCenter('edit')
