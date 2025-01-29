d30c8b0002539f8fc951f5219d652619935e965d
praveenavas <prveena.vasireddy@icf.com>
2025-01-29 10:09:57 -0500

    Fix for ExecuteTestCasesByNonMeasureOwner.cy.ts

--- cypress/e2e/WebInterface/Test Cases/QI-CORE Test Case/Execution/ExecuteTestCasesByNonMeasureOwner.cy.ts
+++ cypress/e2e/WebInterface/Test Cases/QI-CORE Test Case/Execution/ExecuteTestCasesByNonMeasureOwner.cy.ts
@@ -97,7 +97,7 @@ describe('Ability to run valid test cases whether or not the user is the owner o
             cy.get(TestCasesPage.createTestCaseDescriptionInput).type(testCaseDescription)
             cy.get(TestCasesPage.createTestCaseGroupInput).should('exist')
             cy.get(TestCasesPage.createTestCaseGroupInput).should('be.visible')
-            cy.get(TestCasesPage.createTestCaseGroupInput).type(testCaseSeries).type('{enter}')
+            cy.get(TestCasesPage.createTestCaseGroupInput).type(testCaseSeries, {delay:50}).type('{enter}')
 
             TestCasesPage.clickCreateTestCaseButton()
 
