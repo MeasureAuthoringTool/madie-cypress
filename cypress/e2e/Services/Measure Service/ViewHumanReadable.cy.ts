d390eeadeb8e77008c88e2704059ced4e386f7ea
bwelch8105 <brandon.welch@semanticbits.com>
2025-01-30 14:55:56 -0600

    This PR corrects some mistakes that were merged into master branch from last PR. Basically, in this PR, a new test was added to the existing ViewHumanReadable test file, a previously deleted test was added back to a file, and the new test file added from last PR was removed.

--- cypress/e2e/Services/Measure Service/ViewHumanReadable.cy.ts
+++ cypress/e2e/Services/Measure Service/ViewHumanReadable.cy.ts
@@ -1,7 +1,17 @@
-import {CreateMeasurePage} from "../../../Shared/CreateMeasurePage"
-import {Utilities} from "../../../Shared/Utilities"
-import {MeasureCQL} from "../../../Shared/MeasureCQL"
-import {MeasureGroupPage} from "../../../Shared/MeasureGroupPage"
+import { CreateMeasurePage } from "../../../Shared/CreateMeasurePage"
+import { Utilities } from "../../../Shared/Utilities"
+import { MeasureCQL } from "../../../Shared/MeasureCQL"
+import { MeasureGroupPage } from "../../../Shared/MeasureGroupPage"
+import { OktaLogin } from "../../../Shared/OktaLogin"
+import { MeasuresPage } from "../../../Shared/MeasuresPage"
+import { EditMeasurePage } from "../../../Shared/EditMeasurePage"
+import { CQLEditorPage } from "../../../Shared/CQLEditorPage"
+
+
+let newMeasureName = ''
+let newCQLLibraryName = ''
+
+let qdmMeasureCQLVm = MeasureCQL.CQLQDMObservationRun
 
 let randValue = (Math.floor((Math.random() * 1000) + 1))
 let measureName = 'TestMeasure' + Date.now() + randValue
@@ -121,6 +131,72 @@ describe('Measure Service: View Human Readable for Draft QDM Measure', () => {
                 }).then((response) => {
                     expect(response.status).to.eql(200)
                     expect(response.body).not.empty
+                })
+            })
+        })
+    })
+})
+
+describe('Measure Service: View Human readable for Versioned QDM Measure', () => {
+
+    newMeasureName = measureName + 1 + randValue
+    newCQLLibraryName = cqlLibraryName + 1 + randValue
+
+    beforeEach('Create Measure', () => {
+        sessionStorage.clear()
+        cy.clearAllCookies()
+        cy.clearLocalStorage()
+        CreateMeasurePage.CreateQDMMeasureWithBaseConfigurationFieldsAPI(newMeasureName, newCQLLibraryName, 'Cohort', false, qdmMeasureCQLVm)
+        MeasureGroupPage.CreateCohortMeasureGroupAPI(false, false, 'Initial Population')
+        OktaLogin.Login()
+        MeasuresPage.actionCenter('edit')
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+        OktaLogin.UILogout()
+    })
+
+    afterEach('Clean up', () => {
+        sessionStorage.clear()
+        cy.clearAllCookies()
+        cy.clearLocalStorage()
+
+        Utilities.deleteVersionedMeasure(newMeasureName, newCQLLibraryName)
+    })
+
+    it('Successful export of a versioned QDM Measure', () => {
+
+        //version measure
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((measureId) => {
+                cy.request({
+                    url: '/api/measures/' + measureId + '/version?versionType=major',
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value
+                    },
+                    method: 'PUT'
+                }).then((response) => {
+                    expect(response.status).to.eql(200)
+                    expect(response.body.version).to.eql('1.0.000')
+                })
+            })
+        })
+
+        //export measure
+        cy.getCookie('accessToken').then((accessToken) => {
+            cy.readFile('cypress/fixtures/measureId').should('exist').then((id) => {
+                cy.request({
+                    url: '/api/humanreadable/' + id,
+                    method: 'GET',
+                    headers: {
+                        authorization: 'Bearer ' + accessToken.value
+                    }
+                }).then((response) => {
+                    console.log(response)
+                    expect(response.status).to.eql(200)
+                    expect(response.body).is.not.null
+                    expect(response.body).contains('<html')
                 })
             })
         })
