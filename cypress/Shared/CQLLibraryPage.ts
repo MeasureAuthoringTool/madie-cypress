d929b5eebe79482daaa0903f4167a956f1f253c4
Colin Sulllivan <n119493@icf.com>
2025-03-11 15:53:49 -0500

    feat: updates for MAT-7194

--- cypress/Shared/CQLLibraryPage.ts
+++ cypress/Shared/CQLLibraryPage.ts
@@ -2,10 +2,13 @@ import { Header } from "./Header"
 import { Environment } from "./Environment"
 import { Utilities } from "./Utilities"
 import { v4 as uuidv4 } from 'uuid'
+import { CQLLibrariesPage } from "./CQLLibrariesPage"
 
 export enum EditLibraryActions {
 
-    delete
+    delete,
+    version,
+    draft
 }
 
 
@@ -53,10 +56,14 @@ export class CQLLibraryPage {
     public static readonly cqlLibraryExperimentalChkBox = '[id="experimental"]'
     public static readonly editLibraryOwnershipError = '[id="content"]'
     public static readonly editSavedLibraryAlias = '[data-testid="library-alias-input"]'
+    public static readonly libraryInfoPanel = '#page-header'
+    public static readonly draftBubble = '[data-testid="draft-bubble"]'
 
     // action center
     public static readonly actionCenterButton = '[data-testid="action-center-actual-icon"]'
     public static readonly actionCenterDelete = '[data-testid="DeleteLibrary"]'
+    public static readonly actionCenterVersion = '[data-testid="VersionLibrary"]'
+    public static readonly actionCenterDraft = '[data-testid="DraftLibrary"]'
 
     //CQL Editor
     public static readonly cqlLibraryEditorTextBox = '.ace_content'
@@ -430,6 +437,31 @@ export class CQLLibraryPage {
                 cy.get(this.cqlLibraryDeleteDialogContinueBtn).click()
                 break
             }
+            case EditLibraryActions.version: {
+
+                cy.get(this.actionCenterVersion).should('be.visible')
+                cy.get(this.actionCenterVersion).should('be.enabled')
+                cy.get(this.actionCenterVersion).click()
+
+                // click major & continue
+                cy.get('input[value="major"]').check()
+                cy.get(CQLLibrariesPage.createVersionContinueButton).should('be.visible')
+                cy.get(CQLLibrariesPage.createVersionContinueButton).click()
+                break
+            }
+            case EditLibraryActions.draft: {
+                
+                cy.get(this.actionCenterDraft).should('be.visible')
+                cy.get(this.actionCenterDraft).should('be.enabled')
+                cy.get(this.actionCenterDraft).click()
+
+                cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).should('be.enabled')
+        
+                cy.get(CQLLibrariesPage.createDraftContinueBtn).should('be.visible')
+                cy.get(CQLLibrariesPage.createDraftContinueBtn).click()
+
+            }
+
             default: { }
         }
     }
--- cypress/e2e/WebInterface/CQL Library/VersionAndDraft/AddDraftToVersionedCQLLibrary.cy.ts
+++ cypress/e2e/WebInterface/CQL Library/VersionAndDraft/AddDraftToVersionedCQLLibrary.cy.ts
@@ -1,113 +1,21 @@
 import { OktaLogin } from "../../../../Shared/OktaLogin"
-import { CQLLibraryPage } from "../../../../Shared/CQLLibraryPage"
+import { CQLLibraryPage, EditLibraryActions } from "../../../../Shared/CQLLibraryPage"
 import { CQLLibrariesPage } from "../../../../Shared/CQLLibrariesPage"
 import { Header } from "../../../../Shared/Header"
 import { Utilities } from "../../../../Shared/Utilities"
 
-let CqlLibraryOne = ''
-let updatedCqlLibraryName = ''
-let CQLLibraryPublisher = 'SemanticBits'
-
-describe('Add Draft to CQL Library', () => {
-
-    beforeEach('Create CQL Library and Login', () => {
-        //Create CQL Library with Regular User
-        CqlLibraryOne = 'TestLibrary1' + Date.now()
-        CQLLibraryPage.createAPICQLLibraryWithValidCQL(CqlLibraryOne, CQLLibraryPublisher)
-
-        OktaLogin.Login()
-
-    })
-
-    afterEach('Logout', () => {
-
-        OktaLogin.Logout()
-
-    })
-
-    it('Add Draft to the versioned Library', () => {
-
-        let versionNumber = '1.0.000'
-        updatedCqlLibraryName = 'UpdatedTestLibrary1' + Date.now()
-        let filePath = 'cypress/fixtures/cqlLibraryId'
-
-        cy.get(Header.cqlLibraryTab).click()
-        CQLLibrariesPage.cqlLibraryActionCenter("version")
-        cy.get(CQLLibrariesPage.versionLibraryRadioButton).should('exist')
-        cy.get(CQLLibrariesPage.versionLibraryRadioButton).should('be.enabled')
-        cy.get(CQLLibrariesPage.versionLibraryRadioButton).eq(0).click()
-
-        cy.get(CQLLibrariesPage.createVersionContinueButton).should('exist')
-        cy.get(CQLLibrariesPage.createVersionContinueButton).should('be.visible')
-        cy.get(CQLLibrariesPage.createVersionContinueButton).click()
-        cy.get(CQLLibrariesPage.VersionDraftMsgs).should('contain.text', 'New version of CQL Library is Successfully created')
-        CQLLibrariesPage.validateVersionNumber(CqlLibraryOne, versionNumber)
-        cy.log('Version Created Successfully')
-
-        CQLLibrariesPage.cqlLibraryActionCenter("draft")
-        cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).should('exist')
-        cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).should('be.visible')
-        cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).should('be.enabled')
-        cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).clear().type(updatedCqlLibraryName)
-
-        cy.get(CQLLibrariesPage.createDraftContinueBtn).should('exist')
-        cy.get(CQLLibrariesPage.createDraftContinueBtn).should('be.visible')
-        cy.get(CQLLibrariesPage.createDraftContinueBtn).should('be.enabled')
-
-        //intercept draft id once library is drafted
-        cy.readFile(filePath).should('exist').then((fileContents) => {
-            cy.intercept('POST', '/api/cql-libraries/draft/' + fileContents).as('draft')
-        })
-        cy.get(CQLLibrariesPage.createDraftContinueBtn).click()
-        cy.wait('@draft', { timeout: 60000 }).then((request) => {
-            cy.writeFile(filePath, request.response.body.id)
-        })
-        cy.get(CQLLibrariesPage.VersionDraftMsgs).should('contain.text', 'New Draft of CQL Library is Successfully created')
-        cy.get(CQLLibrariesPage.cqlLibraryVersionList).should('contain', 'Draft 1.0.000')
-        cy.log('Draft Created Successfully')
-
-        //navigate into the Library's detail / edit page
-        cy.get(Header.cqlLibraryTab).click()
-        //<a tabindex="0" role="tab" aria-selected="true" name="cql-libraries" aria-label="CQL Library" id="cql-library-main-nav-bar-tab" data-testid="main-nav-bar-cql-library" class="MuiButtonBase-root MuiTab-root MuiTab-textColorPrimary Mui-selected css-1q7l0ib active" href="/cql-libraries" aria-current="page" data-cypress-el="true">Libraries<span class="MuiTouchRipple-root css-w0pj6f"></span></a>
-        CQLLibrariesPage.clickEditforCreatedLibrary()
-
-        //confirm that CQL Editor window is not empty
-        cy.get(CQLLibraryPage.cqlLibraryEditorTextBox)
-            .find('[class="ace_layer ace_text-layer"]')
-            .find('[class="ace_line_group"]')
-            .find('[class=ace_line]')
-            .find('[class="ace_identifier"]')
-            .should('exist')
-    })
-
-})
-
+let CqlLibraryOne: string
+const CQLLibraryPublisher = 'SemanticBits'
+const versionNumber = '1.0.000'
+const filePath = 'cypress/fixtures/cqlLibraryId'
 
 describe('Action Center Buttons - Add Draft to CQL Library', () => {
 
     beforeEach('Create CQL Library and Login', () => {
-        //Create CQL Library with Regular User
-        CqlLibraryOne = 'TestLibrary1' + Date.now()
+
+        CqlLibraryOne = 'DraftingLibrary' + Date.now()
         CQLLibraryPage.createAPICQLLibraryWithValidCQL(CqlLibraryOne, CQLLibraryPublisher)
 
-    })
-
-    afterEach('Logout', () => {
-
-        OktaLogin.Logout()
-
-    })
-
-    it('Add Draft to the versioned Library using Action Center Buttons', () => {
-
-        let versionNumber = '1.0.000'
-        updatedCqlLibraryName = 'UpdatedTestLibrary1' + Date.now()
-        let filePath = 'cypress/fixtures/cqlLibraryId'
-
-        cy.clearCookies()
-        cy.clearLocalStorage()
-        //Version CQL Library
-        cy.setAccessTokenCookie()
         cy.getCookie('accessToken').then((accessToken) => {
             cy.readFile(filePath).should('exist').then((cqlLibraryId) => {
                 cy.request({
@@ -124,6 +32,14 @@ describe('Action Center Buttons - Add Draft to CQL Library', () => {
                 })
             })
         })
+    })
+
+    afterEach('Logout', () => {
+
+        OktaLogin.Logout()
+    })
+
+    it('Add Draft to the versioned Library from My Libraries', () => {
 
         //Add Draft to Versioned Library
         OktaLogin.Login()
@@ -132,7 +48,7 @@ describe('Action Center Buttons - Add Draft to CQL Library', () => {
         cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).should('exist')
         cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).should('be.visible')
         cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).should('be.enabled')
-        cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).clear().type(updatedCqlLibraryName)
+        cy.get(CQLLibrariesPage.updateDraftedLibraryTextBox).clear().type(CqlLibraryOne)
 
         cy.get(CQLLibrariesPage.createDraftContinueBtn).should('exist')
         cy.get(CQLLibrariesPage.createDraftContinueBtn).should('be.visible')
@@ -151,35 +67,34 @@ describe('Action Center Buttons - Add Draft to CQL Library', () => {
         cy.log('Draft Created Successfully')
 
         //Delete Draft Library
-        Utilities.deleteLibrary(updatedCqlLibraryName)
+        Utilities.deleteLibrary(CqlLibraryOne)
+    })
+
+    it('Add Draft to the versioned Library from Edit Library screen', () => {
+         //intercept draft id once library is drafted
+         cy.readFile(filePath).should('exist').then((fileContents) => {
+            cy.intercept('POST', '/api/cql-libraries/draft/' + fileContents).as('draft')
+        })
+
+        OktaLogin.Login()
+        cy.get(Header.cqlLibraryTab).click()
+
+        CQLLibrariesPage.clickViewforCreatedLibrary()
+        CQLLibraryPage.actionCenter(EditLibraryActions.draft)
+
+        cy.wait('@draft', { timeout: 60000 }).then((request) => {
+            cy.writeFile(filePath, request.response.body.id)
+        })
+
+        cy.get(CQLLibraryPage.genericSuccessMessage).should('contain.text', 'New Draft of CQL Library is Successfully created')
+        cy.get(CQLLibraryPage.draftBubble).should('be.visible')
+        cy.log('Draft Created Successfully')
+
+        //Delete Draft Library
+        Utilities.deleteLibrary(CqlLibraryOne)
     })
 
     it('Non Measure Owner unable to add Draft to the versioned Library using Action Center Buttons', () => {
-
-        let versionNumber = '1.0.000'
-        updatedCqlLibraryName = 'UpdatedTestLibrary1' + Date.now()
-        let filePath = 'cypress/fixtures/cqlLibraryId'
-
-        cy.clearCookies()
-        cy.clearLocalStorage()
-        //Version CQL Library
-        cy.setAccessTokenCookie()
-        cy.getCookie('accessToken').then((accessToken) => {
-            cy.readFile(filePath).should('exist').then((cqlLibraryId) => {
-                cy.request({
-                    url: '/api/cql-libraries/version/' + cqlLibraryId + '?isMajor=true',
-                    method: 'PUT',
-                    headers: {
-                        authorization: 'Bearer ' + accessToken.value
-                    }
-
-                }).then((response) => {
-                    expect(response.status).to.eql(200)
-                    expect(response.body.version).to.eql(versionNumber)
-
-                })
-            })
-        })
 
         //Verify that the Draft button is disabled for Non Measure owner
         OktaLogin.AltLogin()
