cbedce27df4557a5111f82d19ba74224449aac6e
Colin Sulllivan <n119493@icf.com>
2025-03-14 14:41:21 -0500

    fix: restore qdm testcasecopytonewmeasure

--- cypress/Shared/CreateMeasurePage.ts
+++ cypress/Shared/CreateMeasurePage.ts
@@ -559,10 +559,12 @@ export class CreateMeasurePage {
                 if (measureNumber > 0) {
                     cy.writeFile('cypress/fixtures/measureId' + measureNumber, response.body.id)
                     cy.writeFile('cypress/fixtures/measureSetId' + measureNumber, response.body.measureSetId)
+                    cy.writeFile('cypress/fixtures/versionId' + measureNumber, response.body.versionId)
                 }
                 else {
                     cy.writeFile('cypress/fixtures/measureId', response.body.id)
                     cy.writeFile('cypress/fixtures/measureSetId', response.body.measureSetId)
+                    cy.writeFile('cypress/fixtures/versionId', response.body.versionId)
                 }
             })
         })
