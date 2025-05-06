0c710bf9e90de45f9b9674491764befea7fd8957
Colin Sulllivan <n119493@icf.com>
2025-05-06 12:26:41 -0500

    feat: remove duplicated cql from several tests

--- cypress/Shared/MeasureCQL.ts
+++ cypress/Shared/MeasureCQL.ts
@@ -39,18 +39,14 @@ export class MeasureCQL {
         'define "Initial PopulationOne":\n' +
         'true\n'
 
-    public static readonly zipfileExportQICore = 'library TestLibrary1678378360032 version \'0.0.000\'\n' +
-        '\n' +
-        'using QICore version \'4.1.1\'\n' +
-        '\n' +
+    public static readonly zipfileExportQICore = 'library TestLibrary1678378360032 version \'0.0.000\'\n\n' +
+        'using QICore version \'4.1.1\'\n\n' +
         'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
         'include SupplementalDataElements version \'3.5.000\' called SDE\n' +
-        'include CQMCommon version \'1.0.000\' called CQMCommon \n' +
-        'include FHIRCommon version \'4.1.000\' called FHIRCommon\n' +
-        '\n' + '\n' +
-        'codesystem "SNOMEDCT": \'http://snomed.info/sct\' \n' +
-        'codesystem "ActCode": \'http://terminology.hl7.org/CodeSystem/v3-ActCode\'  \n' +
-        '\n' + '\n' + '\n' +
+        'include CQMCommon version \'1.0.000\' called CQMCommon\n' +
+        'include FHIRCommon version \'4.1.000\' called FHIRCommon\n\n' +
+        'codesystem "SNOMEDCT": \'http://snomed.info/sct\'\n' +
+        'codesystem "ActCode": \'http://terminology.hl7.org/CodeSystem/v3-ActCode\'\n\n' +
         'valueset "Care Services in Long-Term Residential Facility": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1014\' \n' +
         'valueset "Diabetic Retinopathy": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.327\' \n' +
         'valueset "Level of Severity of Retinopathy Findings": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1283\' \n' +
@@ -60,8 +56,7 @@ export class MeasureCQL {
         'valueset "Nursing Facility Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1012\' \n' +
         'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\' \n' +
         'valueset "Ophthalmological Services": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1285\' \n' +
-        'valueset "Patient Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1008\' \n' +
-        '\n' +
+        'valueset "Patient Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1008\'\n\n' +
         'code "Healthcare professional (occupation)": \'223366009\' from "SNOMEDCT" display \'Healthcare professional (occupation)\'\n' +
         'code "Medical practitioner (occupation)": \'158965000\' from "SNOMEDCT" display \'Medical practitioner (occupation)\'\n' +
         'code "Ophthalmologist (occupation)": \'422234006\' from "SNOMEDCT" display \'Ophthalmologist (occupation)\'\n' +
@@ -69,30 +64,21 @@ export class MeasureCQL {
         'code "Physician (occupation)": \'309343006\' from "SNOMEDCT" display \'Physician (occupation)\'\n' +
         'code "virtual": \'VR\' from "ActCode" display \'virtual\'\n' +
         'code "Macular edema absent (situation)": \'428341000124108\' from "SNOMEDCT" display \'Macular edema absent (situation)\'\n' +
-        'code "AMB" : \'AMB\' from "ActCode" display \'Ambulatory\'\n' +
-        '\n' +
-        'parameter "Measurement Period" Interval<DateTime>\n' +
-        '\n' +
-        'context Patient\n' +
-        '\n' +
+        'code "AMB" : \'AMB\' from "ActCode" display \'Ambulatory\'\n\n' +
+        'parameter "Measurement Period" Interval<DateTime>\n\n' +
+        'context Patient\n\n' +
         'define "SDE Ethnicity":\n' +
-        '  SDE."SDE Ethnicity"\n' +
-        '\n' +
+        '  SDE."SDE Ethnicity"\n\n' +
         'define "SDE Payer":\n' +
-        '  SDE."SDE Payer"\n' +
-        '\n' +
+        '  SDE."SDE Payer"\n\n' +
         'define "SDE Race":\n' +
-        '  SDE."SDE Race"\n' +
-        '\n' +
+        '  SDE."SDE Race"\n\n' +
         'define "SDE Sex":\n' +
-        '  SDE."SDE Sex"\n' +
-        '\n' + '\n' + '\n' +
+        '  SDE."SDE Sex"\n\n' +
         'define "ipp":\n' +
-        'true\n' +
-        '\n' +
+        'true\n\n' +
         'define "num":\n' +
-        'true\n' +
-        '\n' +
+        'true\n\n' +
         'define "denom":\n' +
         'true'
 
--- cypress/e2e/WebInterface/Measure/QI Core Measure Export/MeasureExportNotTheOwner.cy.ts
+++ cypress/e2e/WebInterface/Measure/QI Core Measure Export/MeasureExportNotTheOwner.cy.ts
@@ -2,76 +2,18 @@ import { CreateMeasurePage } from "../../../../Shared/CreateMeasurePage"
 import { OktaLogin } from "../../../../Shared/OktaLogin"
 import { MeasuresPage } from "../../../../Shared/MeasuresPage"
 import { MeasureGroupPage } from "../../../../Shared/MeasureGroupPage"
+import { MeasureCQL } from "../../../../Shared/MeasureCQL"
+import { EditMeasurePage } from "../../../../Shared/EditMeasurePage"
+import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
+import { Utilities } from "../../../../Shared/Utilities"
 
-let measureNameTimeStamp = 'TestMeasure' + Date.now()
-let measureName = measureNameTimeStamp
+let measureName = 'TestMeasure' + Date.now()
 let CqlLibraryName = 'TestLibrary' + Date.now()
 const path = require('path')
 const downloadsFolder = Cypress.config('downloadsFolder')
 const { deleteDownloadsFolderBeforeAll } = require('cypress-delete-downloads-folder')
 
-let measureCQL = 'library TestLibrary1678378360032 version \'0.0.000\'\n' +
-    '\n' +
-    'using QICore version \'4.1.1\'\n' +
-    '\n' +
-    'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
-    'include SupplementalDataElements version \'3.1.000\' called SDE\n' +
-    'include CQMCommon version \'1.0.000\' called CQMCommon \n' +
-    'include FHIRCommon version \'4.1.000\' called FHIRCommon\n' +
-    '\n' +
-    '\n' +
-    'codesystem "SNOMEDCT": \'http://snomed.info/sct\' \n' +
-    'codesystem "ActCode": \'http://terminology.hl7.org/CodeSystem/v3-ActCode\'  \n' +
-    '\n' +
-    '\n' +
-    '\n' +
-    'valueset "Care Services in Long-Term Residential Facility": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1014\' \n' +
-    'valueset "Diabetic Retinopathy": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.327\' \n' +
-    'valueset "Level of Severity of Retinopathy Findings": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1283\' \n' +
-    'valueset "Macular Edema Findings Present": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1320\' \n' +
-    'valueset "Macular Exam": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1251\' \n' +
-    'valueset "Medical Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1007\' \n' +
-    'valueset "Nursing Facility Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1012\' \n' +
-    'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\' \n' +
-    'valueset "Ophthalmological Services": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1285\' \n' +
-    'valueset "Patient Reason": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.526.3.1008\' \n' +
-    '\n' +
-    'code "Healthcare professional (occupation)": \'223366009\' from "SNOMEDCT" display \'Healthcare professional (occupation)\'\n' +
-    'code "Medical practitioner (occupation)": \'158965000\' from "SNOMEDCT" display \'Medical practitioner (occupation)\'\n' +
-    'code "Ophthalmologist (occupation)": \'422234006\' from "SNOMEDCT" display \'Ophthalmologist (occupation)\'\n' +
-    'code "Optometrist (occupation)": \'28229004\' from "SNOMEDCT" display \'Optometrist (occupation)\'\n' +
-    'code "Physician (occupation)": \'309343006\' from "SNOMEDCT" display \'Physician (occupation)\'\n' +
-    'code "virtual": \'VR\' from "ActCode" display \'virtual\'\n' +
-    'code "Macular edema absent (situation)": \'428341000124108\' from "SNOMEDCT" display \'Macular edema absent (situation)\'\n' +
-    'code "AMB" : \'AMB\' from "ActCode" display \'Ambulatory\'\n' +
-    '\n' +
-    'parameter "Measurement Period" Interval<DateTime>\n' +
-    '\n' +
-    'context Patient\n' +
-    '\n' +
-    'define "SDE Ethnicity":\n' +
-    '  SDE."SDE Ethnicity"\n' +
-    '\n' +
-    'define "SDE Payer":\n' +
-    '  SDE."SDE Payer"\n' +
-    '\n' +
-    'define "SDE Race":\n' +
-    '  SDE."SDE Race"\n' +
-    '\n' +
-    'define "SDE Sex":\n' +
-    '  SDE."SDE Sex"\n' +
-    '\n' +
-    '\n' +
-    '\n' +
-    'define "Initial Population":\n' +
-    'true\n' +
-    '\n' +
-    'define "Num":\n' +
-    'true\n' +
-    '\n' +
-    'define "Denom":\n' +
-    'true'
-
+let measureCQL = MeasureCQL.zipfileExportQICore
 
 describe('FHIR Measure Export, Not the Owner', () => {
 
@@ -79,14 +21,18 @@ describe('FHIR Measure Export, Not the Owner', () => {
 
     before('Create New Measure and Login', () => {
 
-        //Create New Measure
         CreateMeasurePage.CreateQICoreMeasureAPI(measureName, CqlLibraryName, measureCQL)
-        //create Measure Group
-        MeasureGroupPage.CreateProportionMeasureGroupAPI(null, false, 'Initial Population', '', '',
-            'Num', '', 'Denom', 'boolean')
+        MeasureGroupPage.CreateProportionMeasureGroupAPI(null, false, 'ipp', '', '', 'num', '', 'denom', 'boolean')
+        OktaLogin.Login()
+        MeasuresPage.actionCenter('edit')
+        cy.get(EditMeasurePage.cqlEditorTab).click()
+        cy.get(EditMeasurePage.cqlEditorTextBox).type('{moveToEnd}{enter}')
+        cy.get(EditMeasurePage.cqlEditorSaveButton).click()
+        cy.get(CQLEditorPage.successfulCQLSaveNoErrors).should('be.visible')
+        Utilities.waitForElementDisabled(EditMeasurePage.cqlEditorSaveButton, 16500)
+        OktaLogin.Logout()
 
         OktaLogin.AltLogin()
-
     })
 
     it('Validate the zip file Export is downloaded for FHIR Measure', () => {
@@ -101,7 +47,6 @@ describe('FHIR Measure Export, Not the Owner', () => {
         cy.log('Successfully verified zip file export')
 
         OktaLogin.Logout()
-
     })
 
     it('Unzip the downloaded file and verify file types for FHIR Measure', () => {
@@ -113,10 +58,10 @@ describe('FHIR Measure Export, Not the Owner', () => {
             })
 
         //Verify all files exist in exported zip file
-        cy.readFile(path.join(downloadsFolder, 'eCQMTitle4QICore-v0.0.000-FHIR4.zip')).should('contain', 'eCQMTitle4QICore-v0.0.000-FHIR.html')
+        cy.readFile(path.join(downloadsFolder, 'eCQMTitle4QICore-v0.0.000-FHIR4.zip'))
+            .should('contain', 'eCQMTitle4QICore-v0.0.000-FHIR.html')
             .and('contain', 'eCQMTitle4QICore-v0.0.000-FHIR.xml')
             .and('contain', 'eCQMTitle4QICore-v0.0.000-FHIR.json')
-
     })
 })
 
