099cf2138286f847fcb43acf83b60f3f32f0b0ee
praveenavas <prveena.vasireddy@icf.com>
2025-06-03 12:45:46 -0400

    MAT-8687 Last saved time stamp for Test cases

--- cypress/Shared/TestCasesPage.ts
+++ cypress/Shared/TestCasesPage.ts
@@ -646,7 +646,7 @@ export class TestCasesPage {
         Utilities.waitForElementDisabled(TestCasesPage.editTestCaseSaveButton, 6500)
 
         cy.get(this.successMsg).each(msg => {
-            expect(msg.text()).to.be.oneOf(['Test case updated successfully!', 'Test case updated successfully with errors in JSON', 'Test case updated successfully with warnings in JSON'])
+            expect(msg.text()).to.be.oneOf(['Test case updated successfully!', 'Test case updated successfully with errors in JSON', 'Test case updated successfully with warnings in JSON', 'Test case updated successfully! Test case validation has started running, please continue working in MADiE.'])
         })
 
         cy.get(EditMeasurePage.testCasesTab).click()
