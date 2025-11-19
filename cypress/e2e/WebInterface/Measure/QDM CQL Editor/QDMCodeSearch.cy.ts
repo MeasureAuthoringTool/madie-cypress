3ccbd2c97ff3e94149183d933f9496d60453107c
praveenavas <prveena.vasireddy@icf.com>
2025-11-19 12:48:07 -0500

    Fixed timing issue for QDMCodeSearch.cy.ts

--- cypress/e2e/WebInterface/Measure/QDM CQL Editor/QDMCodeSearch.cy.ts
+++ cypress/e2e/WebInterface/Measure/QDM CQL Editor/QDMCodeSearch.cy.ts
@@ -249,7 +249,7 @@ describe('QDM Code Search fields', () => {
         CQLEditorPage.validateSuccessfulCQLUpdate()
     })
 
-    it('Edit Code with Suffix and Version from Saved Codes Grid', () => {
+    it.only('Edit Code with Suffix and Version from Saved Codes Grid', () => {
 
         //Click on Codes tab
         cy.get(CQLEditorPage.codesTab).click()
@@ -279,10 +279,10 @@ describe('QDM Code Search fields', () => {
         cy.get(CQLEditorPage.codesTab).click().wait(1000)
         cy.get(CQLEditorPage.savedCodesTab).should('be.visible')
         cy.get(CQLEditorPage.savedCodesTab).should('be.enabled')
-        cy.get(CQLEditorPage.savedCodesTab).click()
+        cy.get(CQLEditorPage.savedCodesTab).click().wait(2000)
 
         //Edit code
-        Utilities.waitForElementVisible('[data-testid="edit-code-0"]', 60000)
+        Utilities.waitForElementVisible('[data-testid="edit-code-0"]', 70000)
         cy.get('[data-testid="edit-code-0"]').click()
 
         //Code Details Pop up screen
