3307e63ffd103cc072b1532639559e3814594bb6
Ed Rubio <Edward.Rubio@icf.com>
2026-06-22 10:38:05 -0400

    updated workaroud and Library owners Name

--- cypress/e2e/WebInterface/Measure/QI Core CQL Editor/QiCoreLibraryIncludes.cy.ts
+++ cypress/e2e/WebInterface/Measure/QI Core CQL Editor/QiCoreLibraryIncludes.cy.ts
@@ -1,19 +1,17 @@
-import { CreateMeasurePage } from "../../../../Shared/CreateMeasurePage"
-import { OktaLogin } from "../../../../Shared/OktaLogin"
-import { CQLEditorPage } from "../../../../Shared/CQLEditorPage"
-import { Utilities } from "../../../../Shared/Utilities"
-import { MeasureCQL } from "../../../../Shared/MeasureCQL"
-import { MeasuresPage } from "../../../../Shared/MeasuresPage"
-import { EditMeasurePage } from "../../../../Shared/EditMeasurePage"
+import { CreateMeasurePage } from '../../../../Shared/CreateMeasurePage'
+import { OktaLogin } from '../../../../Shared/OktaLogin'
+import { CQLEditorPage } from '../../../../Shared/CQLEditorPage'
+import { Utilities } from '../../../../Shared/Utilities'
+import { MeasureCQL } from '../../../../Shared/MeasureCQL'
+import { MeasuresPage } from '../../../../Shared/MeasuresPage'
+import { EditMeasurePage } from '../../../../Shared/EditMeasurePage'
 
 let measureName = 'QiCoreTestMeasure' + Date.now()
 let CqlLibraryName = 'QiCoreLibrary' + Date.now()
 let measureCQL = MeasureCQL.ICFCleanTest_CQL
 
 describe('Qi-Core Library Includes fields', () => {
-
     beforeEach('Create Measure and Login', () => {
-
         CreateMeasurePage.CreateQICoreMeasureAPI(measureName, CqlLibraryName, measureCQL)
         OktaLogin.SessionLogin()
 
@@ -24,14 +22,11 @@ describe('Qi-Core Library Includes fields', () => {
     })
 
     afterEach('Clean up and Logout', () => {
-
-        
         Utilities.deleteMeasure()
         OktaLogin.UILogout()
     })
 
     it('Search for Qi-Core Included Libraries', () => {
-
         //Click on Includes tab
         cy.get(CQLEditorPage.includesTab).click()
 
@@ -43,28 +38,36 @@ describe('Qi-Core Library Includes fields', () => {
         //Search for QDM Libraries
         cy.get(CQLEditorPage.librarySearchTextBox).clear().type('vte')
         cy.get(CQLEditorPage.librarySearchBtn).click()
-        cy.get(CQLEditorPage.librarySearchTable).should('include.text', 'nameversionownerActionVTE8.10.000yahu1257View ' +
-            '/ ApplyVTE8.9.000yahu1257View / ApplyVTE8.8.000yahu1257View / ApplyVTE8.7.000yahu1257View / ApplyVTE8.6.000yahu1257View ' +
-            '/ ApplyItems per page 5.Items per page5​1 - 5 of 181234')
+        cy.get(CQLEditorPage.librarySearchTable).should(
+            'include.text',
+            'nameversionownerActionVTE8.10.000Yanyan HuView ' +
+                '/ ApplyVTE8.9.000Yanyan HuView / ApplyVTE8.8.000Yanyan HuView / ApplyVTE8.7.000Yanyan HuView / ApplyVTE8.6.000Yanyan HuView ' +
+                '/ ApplyItems per page 5.Items per page5​1 - 5 of 181234',
+        )
     })
 
     it('Apply Qi-Core Included library to the CQL and save', () => {
-
         //Click on Includes tab
         cy.get(CQLEditorPage.includesTab).click()
 
         //Search for Library
         cy.get(CQLEditorPage.librarySearchTextBox).clear().type('vte')
         cy.get(CQLEditorPage.librarySearchBtn).click()
-        cy.get(CQLEditorPage.librarySearchTable).should('include.text', 'nameversionownerActionVTE8.10.000yahu1257View ' +
-            '/ ApplyVTE8.9.000yahu1257View / ApplyVTE8.8.000yahu1257View / ApplyVTE8.7.000yahu1257View / ApplyVTE8.6.000yahu1257View ' +
-            '/ ApplyItems per page 5.Items per page5​1 - 5 of 181234')
+        cy.get(CQLEditorPage.librarySearchTable).should(
+            'include.text',
+            'nameversionownerActionVTE8.10.000Yanyan HuView ' +
+                '/ ApplyVTE8.9.000Yanyan HuView / ApplyVTE8.8.000Yanyan HuView / ApplyVTE8.7.000Yanyan HuView / ApplyVTE8.6.000Yanyan HuView ' +
+                '/ ApplyItems per page 5.Items per page5​1 - 5 of 181234',
+        )
 
         //Apply Library to CQL
         cy.get('[data-testid="edit-button-0"]').click()
         cy.get('[data-testid="library-alias-input"]').type('VTE')
         cy.get('[data-testid="apply-button"]').click()
-        cy.get(EditMeasurePage.successMessage).should('contain.text', 'Library VTE has been successfully added to the CQL.')
+        cy.get(EditMeasurePage.successMessage).should(
+            'contain.text',
+            'Library VTE has been successfully added to the CQL.',
+        )
 
         //Save CQL
         cy.get(CQLEditorPage.saveCQLButton).click()
@@ -72,22 +75,27 @@ describe('Qi-Core Library Includes fields', () => {
     })
 
     it('Verify error messages when same Library/Alias is applied twice', () => {
-
         //Click on Includes tab
         cy.get(CQLEditorPage.includesTab).click()
 
         //Search for Library
         cy.get(CQLEditorPage.librarySearchTextBox).clear().type('vte')
         cy.get(CQLEditorPage.librarySearchBtn).click()
-        cy.get(CQLEditorPage.librarySearchTable).should('include.text', 'nameversionownerActionVTE8.10.000yahu1257View ' +
-            '/ ApplyVTE8.9.000yahu1257View / ApplyVTE8.8.000yahu1257View / ApplyVTE8.7.000yahu1257View / ApplyVTE8.6.000yahu1257View ' +
-            '/ ApplyItems per page 5.Items per page5​1 - 5 of 181234')
+        cy.get(CQLEditorPage.librarySearchTable).should(
+            'include.text',
+            'nameversionownerActionVTE8.10.000Yanyan HuView ' +
+                '/ ApplyVTE8.9.000Yanyan HuView / ApplyVTE8.8.000Yanyan HuView / ApplyVTE8.7.000Yanyan HuView / ApplyVTE8.6.000Yanyan HuView ' +
+                '/ ApplyItems per page 5.Items per page5​1 - 5 of 181234',
+        )
 
         //Apply Library to CQL
         cy.get('[data-testid="edit-button-0"]').click()
         cy.get('[data-testid="library-alias-input"]').type('VTE')
         cy.get('[data-testid="apply-button"]').click()
-        cy.get(EditMeasurePage.successMessage).should('contain.text', 'Library VTE has been successfully added to the CQL.')
+        cy.get(EditMeasurePage.successMessage).should(
+            'contain.text',
+            'Library VTE has been successfully added to the CQL.',
+        )
 
         //Apply same Library again
         cy.get('[data-testid="edit-button-0"]').click()
@@ -98,7 +106,10 @@ describe('Qi-Core Library Includes fields', () => {
         //Apply different Library with duplicate Alias
         cy.get(CQLEditorPage.librarySearchTextBox).clear().type('fhir')
         cy.get(CQLEditorPage.librarySearchBtn).click()
-        cy.get(CQLEditorPage.librarySearchTable).should('include.text', 'nameversionownerActionCDSConnectCommonsForFHIRv4010.1.000pauld@mitre.orgView / ApplyFHIRCommon4.3.000julietrubiniView / ApplyFHIRCommon4.2.000julietrubiniView / ApplyFHIRCommon4.1.000julietrubiniView / ApplyFHIRCommon4.0.000julietrubiniView / ApplyItems per page 5.Items per page5​1 - 5 of 191234')
+        cy.get(CQLEditorPage.librarySearchTable).should(
+            'include.text',
+            'nameversionownerActionCDSConnectCommonsForFHIRv4010.1.000pauld@mitre.orgView / ApplyFHIRCommon4.3.000Juliet RubiniView / ApplyFHIRCommon4.2.000Juliet RubiniView / ApplyFHIRCommon4.1.000Juliet RubiniView / ApplyFHIRCommon4.0.000Juliet RubiniView / ApplyItems per page 5.Items per page5​1 - 5 of 191234',
+        )
         cy.get('[data-testid="edit-button-0"]').click()
         cy.get('[data-testid="library-alias-input"]').type('VTE')
         cy.get('[data-testid="apply-button"]').click()
@@ -106,7 +117,6 @@ describe('Qi-Core Library Includes fields', () => {
     })
 
     it('Verify Included Libraries under Saved Libraries tab', () => {
-
         //Click on Includes tab
         cy.get(CQLEditorPage.includesTab).click()
 
@@ -115,12 +125,10 @@ describe('Qi-Core Library Includes fields', () => {
         cy.get('[data-test-id="row-0"]').find('td').eq(0).should('contain.text', 'FHIRHelpers') //Alias
         cy.get('[data-test-id="row-0"]').find('td').eq(1).should('contain.text', 'FHIRHelpers') //Name
         cy.get('[data-test-id="row-0"]').find('td').eq(2).should('contain.text', '4.1.000') //Version
-        cy.get('[data-test-id="row-0"]').find('td').eq(3).should('contain.text', 'angela.flanagan') //Owner
+        cy.get('[data-test-id="row-0"]').find('td').eq(3).should('contain.text', 'Angela Flanagan') //Owner
     })
 
-
     it('Verify all Qi Core Library versions are displayed while editing saved Libraries', () => {
-
         cy.get(CQLEditorPage.includesTab).click()
 
         //Navigate to Saved Libraries tab
@@ -138,24 +146,27 @@ describe('Qi-Core Library Includes fields', () => {
 
         //Confirm Library versions
         cy.get(CQLEditorPage.versionDropdownBtn).click()
-        cy.get(CQLEditorPage.versionNumberList).should('contain.text', '4.4.0004.3.0004.2.0004.1.0004.0.0003.0.0002.0.0001.0.0000.1.000')
+        cy.get(CQLEditorPage.versionNumberList).should(
+            'contain.text',
+            '4.4.0004.3.0004.2.0004.1.0004.0.0003.0.0002.0.0001.0.0000.1.000',
+        )
     })
 
     // old defect to watch with this scenario https://jira.cms.gov/browse/MAT-9107
     it('Verify error message appears on Includes tab when there is an error in the Measure CQL', () => {
-
-        const brokenCql = 'library SimpleFhirLibrary version \'0.0.004\'\n' +
-        'using QICore version \'4.1.0\'\n' +
-        'include FHIRHelpers version \'4.1.000\' called FHIRHelpers\n' +
-        'codesystem "SNOMEDCT:2017-09": \'http://snomed.info/sct/731000124108\' version \'http://snomed.info/sct/731000124108/version/201709\'\n' +
-        'valueset "Hysterectomy with No Residual Cervix": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.198.12.1014\'\n' +
-        'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\'\n' +
-        'parameter "Measurement Period" Interval<DateTime>\n' +
-        'context Patient\n' +
-        'define "test":\n' +
-        'define "Surgical Absence of Cervix":\n' +
-        '\t[Procedure: "Hysterectomy with No Residual Cervix"] NoCervixHysterectomy\n' +
-        '\t\twhere NoCervixHysterectomy.status = \'completed\''
+        const brokenCql =
+            "library SimpleFhirLibrary version '0.0.004'\n" +
+            "using QICore version '4.1.0'\n" +
+            "include FHIRHelpers version '4.1.000' called FHIRHelpers\n" +
+            "codesystem \"SNOMEDCT:2017-09\": 'http://snomed.info/sct/731000124108' version 'http://snomed.info/sct/731000124108/version/201709'\n" +
+            'valueset "Hysterectomy with No Residual Cervix": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.198.12.1014\'\n' +
+            'valueset "Office Visit": \'http://cts.nlm.nih.gov/fhir/ValueSet/2.16.840.1.113883.3.464.1003.101.12.1001\'\n' +
+            'parameter "Measurement Period" Interval<DateTime>\n' +
+            'context Patient\n' +
+            'define "test":\n' +
+            'define "Surgical Absence of Cervix":\n' +
+            '\t[Procedure: "Hysterectomy with No Residual Cervix"] NoCervixHysterectomy\n' +
+            "\t\twhere NoCervixHysterectomy.status = 'completed'"
 
         //Navigate to Includes tab and verify no error message appears
         cy.get(CQLEditorPage.includesTab).click()
@@ -186,11 +197,13 @@ describe('Qi-Core Library Includes fields', () => {
             }
         })
         cy.get(CQLEditorPage.includesTab).click()
-        cy.get('[data-testid="cql-builder-errors"]').should('contain.text', 'Unable to retrieve CQL builder lookups. Please verify CQL has no errors. If CQL is valid, please contact the help desk.')
+        cy.get('[data-testid="cql-builder-errors"]').should(
+            'contain.text',
+            'Unable to retrieve CQL builder lookups. Please verify CQL has no errors. If CQL is valid, please contact the help desk.',
+        )
 
         //Navigate to Saved Parameters tab
         cy.get(CQLEditorPage.savedLibrariesTab).click().should('contain.text', 'Saved Libraries (0)')
         cy.contains('No Results were found', { timeout: 30000 }).should('be.visible')
     })
 })
-
