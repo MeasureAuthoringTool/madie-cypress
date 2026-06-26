37ef45383bc97041e818835a4e6274ba0d921fdf
Colin Sulllivan <n119493@icf.com>
2026-06-26 09:32:39 -0500

    fix: update for models, and other associated config

--- cypress/e2e/WebInterface/Smoke Tests/CreateCQLLibrary.cy.ts
+++ cypress/e2e/WebInterface/Smoke Tests/CreateCQLLibrary.cy.ts
@@ -1,11 +1,9 @@
 import { OktaLogin } from "../../../Shared/OktaLogin"
 import { CQLLibraryPage } from "../../../Shared/CQLLibraryPage"
-import { Header } from "../../../Shared/Header"
 import { Utilities } from "../../../Shared/Utilities"
-import { CQLLibrariesPage } from "../../../Shared/CQLLibrariesPage"
+import { SupportedModels } from "../../../Shared/CreateMeasurePage"
 
 let CQLLibraryName = ''
-const CQLLibraryPublisher = 'SemanticBits'
 
 describe('Create CQL Library', () => {
 
@@ -16,51 +14,20 @@ describe('Create CQL Library', () => {
 
     afterEach('Logout', () => {
 
-        Utilities.deleteLibrary(CQLLibraryName)
+        Utilities.deleteLibrary()
     })
 
-    it('Navigate to CQL Library Page and create New QI-Core  CQL Library', () => {
+    it('Navigate to CQL Library Page and create New QI-Core 6.0.0 CQL Library', () => {
 
-        CQLLibraryName = 'QICoreCQLLibrary' + Date.now()
+        CQLLibraryName = 'QICore6CQLLibrary' + Date.now()
 
-        CQLLibraryPage.createCQLLibrary(CQLLibraryName, CQLLibraryPublisher)
+        CQLLibraryPage.createCQLLibrary(CQLLibraryName)
     })
 
     it('Navigate to CQL Library Page and create New QDM CQL Library', () => {
-        const currentUser = Cypress.env('selectedUser')
+       
         CQLLibraryName = 'QDMCQLLibrary' + Date.now()
-        const model = 'QDM v5.6'
 
-        cy.get(Header.cqlLibraryTab).should('be.visible')
-        cy.get(Header.cqlLibraryTab).click()
-        cy.get('[data-testid="loading-state"]').should('not.exist')
-
-        Utilities.waitForElementEnabled(CQLLibraryPage.createCQLLibraryBtn, 60000)
-
-        cy.get(CQLLibraryPage.createCQLLibraryBtn).should('be.visible')
-        cy.get(CQLLibraryPage.createCQLLibraryBtn).should('be.enabled')
-        cy.get(CQLLibraryPage.createCQLLibraryBtn).click()
-
-        cy.get(CQLLibraryPage.newCQLLibName).should('be.visible')
-        cy.get(CQLLibraryPage.newCQLLibName).type(CQLLibraryName)
-        Utilities.dropdownSelect(CQLLibraryPage.cqlLibraryModelDropdown, CQLLibraryPage.cqlLibraryModelQDM)
-        cy.get(CQLLibraryPage.cqlLibraryDesc).type('description')
-        cy.get(CQLLibraryPage.cqlLibraryCreatePublisher).should('exist')
-        cy.get(CQLLibraryPage.cqlLibraryCreatePublisher).should('be.visible')
-        cy.get(CQLLibraryPage.cqlLibraryCreatePublisher).type(CQLLibraryPublisher).type('{downArrow}{enter}')
-
-        CQLLibraryPage.clickCreateLibraryButton()
-        Utilities.waitForElementToNotExist('[class="toast success"]', 60000)
-        
-        cy.get(Header.cqlLibraryTab).should('be.visible')
-        cy.get(Header.cqlLibraryTab).click()
-
-        CQLLibrariesPage.validateCQLLibraryName(CQLLibraryName)
-        cy.readFile('cypress/fixtures/' + currentUser + '/cqlLibraryId').should('exist').then((fileContents) => {
-            cy.get('[data-testid=cqlLibrary-button-' + fileContents + '-model-content]').should('contain', model)
-
-        })
-
-        cy.log('QDM CQL Library Created Successfully')
+        CQLLibraryPage.createCQLLibrary(CQLLibraryName, { model: SupportedModels.QDM })
     })
 })
