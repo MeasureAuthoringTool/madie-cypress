026d59c127f9f826f3766bc822c11bb1683229da
Ed Rubio <Edward.Rubio@icf.com>
2026-09-18 13:20:50 -0400

    test: wait for create measure form readiness

--- cypress/Shared/CreateMeasurePage.ts
+++ cypress/Shared/CreateMeasurePage.ts
@@ -69,6 +69,11 @@ export class CreateMeasurePage {
         return altUser ? 'selectedAltUser' : 'selectedUser'
     }
 
+    private static openCreateMeasureForm(): void {
+        cy.get(LandingPage.newMeasureButton, { timeout: 30000 }).should('be.visible').and('be.enabled').click()
+        cy.get(this.measureNameTextbox, { timeout: 30000 }).should('be.visible').and('be.enabled')
+    }
+
     public static clickCreateMeasureButton(): void {
 
         let alias = 'measure' + (Date.now() + 1).toString()
@@ -128,8 +133,7 @@ export class CreateMeasurePage {
             endDate = mpEndDate
         }
 
-        Utilities.waitForElementEnabled(LandingPage.newMeasureButton, 30000)
-        cy.get(LandingPage.newMeasureButton).wait(2000).click()
+        this.openCreateMeasureForm()
         cy.get(this.measureNameTextbox).type(measureName)             
         cy.get(this.measureModelDropdown).click()
         cy.get('[class="MuiList-root MuiList-padding MuiMenu-list css-ubifyk"]').contains(model).click()       
@@ -166,8 +170,7 @@ export class CreateMeasurePage {
             endDate = mpEndDate
         }
 
-        Utilities.waitForElementEnabled(LandingPage.newMeasureButton, 30000)
-        cy.get(LandingPage.newMeasureButton).wait(2000).click()
+        this.openCreateMeasureForm()
         cy.get(this.measureNameTextbox).type(measureName)
         cy.get(this.measureModelDropdown).click()
         cy.get('[class="MuiList-root MuiList-padding MuiMenu-list css-ubifyk"]').contains(model).click()
